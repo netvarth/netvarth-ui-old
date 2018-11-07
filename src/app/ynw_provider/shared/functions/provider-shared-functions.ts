86b509c8548a40285841886d2639d1fd49837ce0
maniev <manikandan.velayudhan@netvarth.com>
2018-11-07 09:38:36 +0530

    shared

--- src/app/ynw_provider/shared/functions/provider-shared-functions.ts
+++ src/app/ynw_provider/shared/functions/provider-shared-functions.ts
@@ -2,48 +2,46 @@ import { Injectable } from '@angular/core';
 import { projectConstants } from '../../../shared/constants/project-constants';
 import { Messages } from '../../../shared/constants/project-messages';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
-import {SharedFunctions} from '../../../shared/functions/shared-functions';
+import { SharedFunctions } from '../../../shared/functions/shared-functions';
 import { AddProviderWaitlistQueuesComponent } from '../../components/add-provider-waitlist-queues/add-provider-waitlist-queues.component';
 import { ProviderWaitlistCheckInCancelPopupComponent } from '../../components/provider-waitlist-checkin-cancel-popup/provider-waitlist-checkin-cancel-popup.component';
 import { AddInboxMessagesComponent } from '../../../shared/components/add-inbox-messages/add-inbox-messages.component';
 import { CommonDataStorageService } from '../../../shared/services/common-datastorage.service';
 @Injectable()
 export class ProviderSharedFuctions {
-  jaldeecoupon_list: any = [];
-
-    constructor(public dialog: MatDialog, private snackBar: MatSnackBar,
+  private activeQueues: any = [];
+  constructor(public dialog: MatDialog, private snackBar: MatSnackBar,
     public shared_functions: SharedFunctions,
     public common_datastorage: CommonDataStorageService) {
 
+  }
+
+  changeProviderQueueStatus(ob, obj, source = 'queue_list') {
+    let chgstatus = '';
+    let chstatusmsg = '';
+
+    if (obj.queueState === 'ENABLED') {
+      chgstatus = 'disable';
+      chstatusmsg = 'disabled';
+    } else {
+      chgstatus = 'enable';
+      chstatusmsg = 'enabled';
     }
+    let msg = this.shared_functions.getProjectMesssages('WAITLIST_QUEUE_CHG_STAT').replace('[qname]', obj.name);
+    msg = msg.replace('[status]', chstatusmsg);
 
-    changeProviderQueueStatus(ob, obj, source = 'queue_list') {
-        let chgstatus = '';
-        let chstatusmsg = '';
-
-        if (obj.queueState === 'ENABLED') {
-          chgstatus = 'disable';
-          chstatusmsg = 'disabled';
-        } else {
-          chgstatus = 'enable';
-          chstatusmsg = 'enabled';
-        }
-        let msg = this.shared_functions.getProjectMesssages('WAITLIST_QUEUE_CHG_STAT').replace('[qname]', obj.name);
-        msg = msg.replace('[status]', chstatusmsg);
-
-        ob.provider_services.changeProviderQueueStatus(obj.id, chgstatus)
-        .subscribe(data => {
-          this.shared_functions.openSnackBar (msg);
+    ob.provider_services.changeProviderQueueStatus(obj.id, chgstatus)
+      .subscribe(data => {
+        this.shared_functions.openSnackBar(msg);
+        this.queueReloadApi(ob, source);
+      },
+        error => {
+          this.shared_functions.openSnackBar(error, { 'panelClass': 'snackbarerror' });
           this.queueReloadApi(ob, source);
         });
-        error => {
-          this.shared_functions.openSnackBar (error, {'panelClass': 'snackbarerror'});
-          this.queueReloadApi(ob, source);
-        }
-    }
+  }
 
-
-    changecouponStatus(ob, obj, source = 'coupons') {
+ changecouponStatus(ob, obj, source = 'coupons') {
       let chgstatus = '';
       let chstatusmsg = '';
       if (obj.couponStatus === 'Active') {
@@ -53,74 +51,77 @@ export class ProviderSharedFuctions {
         chgstatus = 'active';
         chstatusmsg = 'enabled';
       }
-     this.jaldeecoupon_list= ob.provider_services.changecouponStatus(obj.id, chgstatus);   
+     this.jaldeecoupon_list= ob.provider_services.changecouponStatus(obj.id, chgstatus);
+     
+     
   }
-  addEditQueuePopup(ob, type, source, obj = null) {
+  addEditQueuePopup(ob, type, source, obj = null, schedules = null) {
 
-        ob.queuedialogRef = this.dialog.open(AddProviderWaitlistQueuesComponent, {
-            width: '50%',
-            panelClass: ['commonpopupmainclass'],
-            disableClose: true,
-            autoFocus: true,
-            data: {
-              queue : obj,
-              source: source,
-              type : type
-            },
-          });
+    ob.queuedialogRef = this.dialog.open(AddProviderWaitlistQueuesComponent, {
+      width: '50%',
+      panelClass: ['commonpopupmainclass'],
+      disableClose: true,
+      autoFocus: true,
+      data: {
+        queue: obj,
+        source: source,
+        type: type,
+        schedules: schedules
+      },
+    });
 
-          ob.queuedialogRef.afterClosed().subscribe(result => {
-            if (result === 'reloadlist') {
-              this.queueReloadApi(ob, source);
-            }
-          });
-    }
-
-    changeProviderLocationStatusMessage(obj) {
-
-      return new Promise((resolve, reject) => {
-        let chgstatus = '';
-        let chstatusmsg = '';
-        if (obj.status === 'ACTIVE') {
-          chgstatus = 'disable';
-          chstatusmsg = 'disabled';
-        } else {
-          chgstatus = 'enable';
-          chstatusmsg = 'enabled';
-        }
-        let msg = this.shared_functions.getProjectMesssages('WAITLIST_LOCATION_CHG_STATLOCATION').replace('[locname]', obj.place);
-        msg = msg.replace('[status]', chstatusmsg);
-        resolve({msg: msg , chgstatus: chgstatus });
-
-      });
-
-    }
-
-    // openSnackBar(message: string, params: any = []) {
-    //   // const panelclass = (params['panelClass']) ? params['panelClass'] : 'snackbarnormal';
-    //   // const snackBarRef = this.snackBar.open(message, '', {duration: projectConstants.TIMEOUT_DELAY_LARGE, panelClass: panelclass });
-    //   // return snackBarRef;
-    //   return this.shared_functions.openSnackBar(message, params);
-    // }
-
-    queueReloadApi(ob, source = 'queue_list') {
-      if (source === 'queue_list') {
-        ob.getProviderQueues();
-      } else if (source === 'queue_detail') {
-        ob.getQueueDetail();
-      } else if (source === 'location_detail') {
-        ob.getProviderQueues();
+    ob.queuedialogRef.afterClosed().subscribe(result => {
+      if (result === 'reloadlist') {
+        this.queueReloadApi(ob, source);
       }
+    });
+  }
+
+  changeProviderLocationStatusMessage(obj) {
+
+    return new Promise((resolve, reject) => {
+      let chgstatus = '';
+      let chstatusmsg = '';
+      if (obj.status === 'ACTIVE') {
+        chgstatus = 'disable';
+        chstatusmsg = 'disabled';
+      } else {
+        chgstatus = 'enable';
+        chstatusmsg = 'enabled';
+      }
+      let msg = this.shared_functions.getProjectMesssages('WAITLIST_LOCATION_CHG_STATLOCATION').replace('[locname]', obj.place);
+      msg = msg.replace('[status]', chstatusmsg);
+      resolve({ msg: msg, chgstatus: chgstatus });
+
+    });
+
+  }
+
+  // openSnackBar(message: string, params: any = []) {
+  //   // const panelclass = (params['panelClass']) ? params['panelClass'] : 'snackbarnormal';
+  //   // const snackBarRef = this.snackBar.open(message, '', {duration: projectConstants.TIMEOUT_DELAY_LARGE, panelClass: panelclass });
+  //   // return snackBarRef;
+  //   return this.shared_functions.openSnackBar(message, params);
+  // }
+
+  queueReloadApi(ob, source = 'queue_list') {
+    if (source === 'queue_list') {
+      ob.getProviderQueues();
+    } else if (source === 'queue_detail') {
+      ob.getQueueDetail();
+    } else if (source === 'location_detail') { 
+      ob.getProviderQueues();
     }
+  }
 
 
   changeServiceStatus(ob, service) {
 
     let chstatusmsg = '';
     if (service.status === 'ACTIVE') {
-        chstatusmsg = 'disabled';
+      chstatusmsg = 'disabled';
     } else {
-        chstatusmsg = 'enabled';
+      chstatusmsg = 'enabled';
     }
 
     let msg = this.shared_functions.getProjectMesssages('WAITLIST_SERVICE_CHG_STAT').replace('[sername]', service.name);
@@ -162,27 +163,27 @@ export class ProviderSharedFuctions {
     return new Promise((resolve, reject) => {
 
       ob.provider_services.changeProviderWaitlistStatus(waitlist.ynwUuid, action, post_data)
-      .subscribe(
-        data => {
+        .subscribe(
+          data => {
 
-          resolve('changeWaitlistStatusApi');
+            resolve('changeWaitlistStatusApi');
 
-          let status_msg = '';
-          switch (action) {
-            case 'REPORT' : status_msg = '[arrived]'; break;
-            case 'STARTED' : status_msg = '[started]'; break;
-            case 'CANCEL' : status_msg = '[cancelled]'; break;
-            case 'CHECK_IN' : status_msg = '[waitlisted]'; break;
-            case 'DONE': status_msg = '[done]'; break;
+            let status_msg = '';
+            switch (action) {
+              case 'REPORT': status_msg = '[arrived]'; break;
+              case 'STARTED': status_msg = '[started]'; break;
+              case 'CANCEL': status_msg = '[cancelled]'; break;
+              case 'CHECK_IN': status_msg = '[waitlisted]'; break;
+              case 'DONE': status_msg = '[done]'; break;
+            }
+            const msg = this.shared_functions.getProjectMesssages('WAITLIST_STATUS_CHANGE').replace('[status]', status_msg);
+            this.shared_functions.openSnackBar(msg);
+          },
+          error => {
+            this.shared_functions.openSnackBar(error, { 'panelClass': 'snackbarerror' });
+            reject();
           }
-          const msg = this.shared_functions.getProjectMesssages('WAITLIST_STATUS_CHANGE').replace('[status]', status_msg);
-          this.shared_functions.openSnackBar (msg);
-        },
-        error => {
-          this.shared_functions.openSnackBar(error, {'panelClass': 'snackbarerror'});
-          reject();
-        }
-      );
+        );
 
     });
 
@@ -190,7 +191,9 @@ export class ProviderSharedFuctions {
 
   }
 
-  addConsumerInboxMessage(uuid, Cthis?) {
+  addConsumerInboxMessage(waitlist, Cthis?) {
+    const uuid = waitlist.ynwUuid || null;
+    const name = waitlist.consumer.userProfile.firstName + ' ' + waitlist.consumer.userProfile.lastName;
     const terminologies = this.common_datastorage.get('terminologies');
     return new Promise((resolve, reject) => {
       Cthis.sendmsgdialogRef = this.dialog.open(AddInboxMessagesComponent, {
@@ -198,10 +201,11 @@ export class ProviderSharedFuctions {
         panelClass: 'commonpopupmainclass',
         disableClose: true,
         data: {
-          uuid : uuid,
+          uuid: uuid,
           source: 'provider-waitlist',
           type: 'send',
-          terminologies: terminologies
+          terminologies: terminologies,
+          name:name
         }
       });
 
@@ -209,7 +213,7 @@ export class ProviderSharedFuctions {
         if (result === 'reloadlist') {
           resolve();
         } else {
-           reject ();
+          reject();
         }
       });
     });
@@ -217,6 +221,13 @@ export class ProviderSharedFuctions {
 
   getTerminologies() {
 
+  }
+  setActiveQueues(active_queues) {
+    this.activeQueues = active_queues;
+  }
+
+  getActiveQueues() {
+    return this.activeQueues;
   }
 
 }
