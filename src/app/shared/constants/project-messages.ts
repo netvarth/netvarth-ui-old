b309c07a1aae390e4a87e45fe50bb1162e267c3c
maniev <manikandan.velayudhan@netvarth.com>
2019-02-02 14:08:02 +0530

    wizard change

--- src/app/shared/constants/project-messages.ts
+++ src/app/shared/constants/project-messages.ts
@@ -1030,7 +1030,7 @@ export const Messages = {
   'WIZ_VIEW_IN_PUBLIC_SEARCH_CAP' : 's will view this in Jaldee Online',
   'WIZ_PROFILE_SUMMARY_CAP' : 'Profile Summary',
   'WIZ_NEXT_CAP' : 'Next',
-  'WIZ_ADD_MORE_SERVICE_IN_THE_SETT_CAP' : 'You can add more services in the Settings -> Services, after you create your profile.',
+  'WIZ_ADD_MORE_SERVICE_IN_THE_SETT_CAP' : 'You can update the default created service details here. You can add more services in the Settings -> Services, after you create your profile.',
   'WIZ_ADD_MORE_QUEUE_IN_THE_SETT_CAP' : 'The service s1 will be served for all working hours',
   'WIZ_LOCATION_CAP' : 'Location',
   'WIZ_ADD_MORE_LOC_IN_THE_SETT_CAP' : 'You can add more locations in the Settings -> Location, after you create your profile.',
