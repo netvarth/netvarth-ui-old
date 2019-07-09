55b50a47940c3a9605bb319283e6cc62f7f01e7c
archana <archana@archana-HP-Pavilion-17-Notebook-PC>
2019-07-09 16:27:30 +0530

    learnmore

--- src/app/ynw_provider/components/learnmore/finance/finance.component.ts
+++ src/app/ynw_provider/components/learnmore/finance/finance.component.ts
@@ -1,17 +1,14 @@
 import { Component, Input, OnChanges } from '@angular/core';
-
 @Component({
     selector: 'app-finance-learnmore',
     templateUrl: './finance.component.html'
 })
-
 export class FinanceComponent implements OnChanges {
     @Input() parent;
     @Input() child;
     parentContent;
     childContent;
     activePrice = '';
-
     ngOnChanges() {
         this.parentContent = this.parent;
         this.childContent = this.child;
--- src/app/ynw_provider/components/learnmore/foodjoints/foodjoints.component.ts
+++ src/app/ynw_provider/components/learnmore/foodjoints/foodjoints.component.ts
@@ -1,17 +1,14 @@
 import { Component, Input, OnInit, OnChanges } from '@angular/core';
-
 @Component({
     selector: 'app-foodjoints-learnmore',
     templateUrl : './foodjoints.component.html'
 })
-
 export class FoodJointComponent implements OnChanges {
     @Input() parent;
     @Input() child;
     parentContent;
     childContent;
     activePrice = '';
-
     ngOnChanges() {
         this.parentContent = this.parent;
         this.childContent = this.child;
