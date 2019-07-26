9760050eb04b865a3bcc2f8ac77accce2a747dd2
beena <beena@DESKTOP-3OUKDGT>
2019-07-26 16:19:20 +0530

    bug fixes

--- src/app/shared/constants/project-messages.ts
+++ src/app/shared/constants/project-messages.ts
@@ -101,7 +101,7 @@ export const Messages = {
   'SERVICE_ADDED2': 'Please add this service to the relevant working hours by editing it.',
   'SERVICE_IMAGE_DELETED': 'Service image deleted successfully',
   'SERVICE_IMAGE_UPLOADED': 'Service image(s) uploaded successfully',
-  'SERVICE_PRE_PAY_ERROR': 'Payment settings needs to be completed before enabling the Pre-Payment option',
+  'SERVICE_PRE_PAY_ERROR': 'Enable [customer] self pay button under payment settings to collect prepayment for this service.',
   'SERVICE_TAX_ZERO_ERROR': 'Tax settings need to be completed for tax to be applicable',
   'WAITLIST_LOCATION_CREATED': 'Location created successfully',
   'WAITLIST_LOCATION_UPDATED': 'Location updated successfully',
