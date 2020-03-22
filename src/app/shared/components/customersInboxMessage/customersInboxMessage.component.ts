551deafc2e72d36641fa16135b7173118298bd6a
priya <priya.c@netvarth.com>
2020-03-22 21:09:23 +0530

    delete

--- src/app/shared/components/customersInboxMessage/customersInboxMessage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-
-@Component({
-    selector:'app-customersInboxMessage',
-    templateUrl:'./customersInboxMessage.component.html'
-})
-export class customersInboxMessageComponent{
-}
