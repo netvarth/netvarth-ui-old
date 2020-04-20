3cb0ff6a175a42d0e70e56afbbbf7dfc89c8a6a6
shiva <shiva@shivareddy-Latitude-3450>
2020-04-20 09:38:33 +0530

    DOnation manager setting routing

--- src/app/business/modules/settings/donationmanager/donation.component.ts
+++ src/app/business/modules/settings/donationmanager/donation.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-donationmanager',
     templateUrl: './donation.component.html'
 })
 export class donationcomponent {
+
+  breadcrumbs_init = [
+    {
+      url: '/provider/settings',
+      title: 'Settings'
+    },
+    {
+      title: 'Donation Manager ',
+    }
+  ];
+  breadcrumbs = this.breadcrumbs_init;
+
+  constructor(private router: Router,  
+    private routerobj: Router)
+    {
+    // this.customer_label = this.shared_functions.getTerminologyTerm('customer');
+  }
+
+ 
+  gotocauses() {
+     this.router.navigate(['provider', 'settings', 'donationmanager']);
+
+  }
+//   learnmore_clicked(mod, e) {
+//     e.stopPropagation();
+//     this.routerobj.navigate(['/provider/' + this.domain + '/billing->' + mod]);
+//   }
+// }
+
 
   
 }
