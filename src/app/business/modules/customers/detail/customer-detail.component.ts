c773f0ce86b2ccad32952cb679bd1a55429a2d0c
user <user@192.168.18.2>
2020-05-21 15:28:41 +0530

    customer details

--- src/app/business/modules/customers/detail/customer-detail.component.ts
+++ src/app/business/modules/customers/detail/customer-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FormMessageDisplayService } from '../../../../shared/modules/form-message-display/form-message-display.service';
 import { ProviderServices } from '../../../../ynw_provider/services/provider-services.service';
 import { Messages } from '../../../../shared/constants/project-messages';
@@ -44,26 +44,28 @@ export class CustomerDetailComponent implements OnInit {
     firstName: any;
     lastName: any;
     dob: any;
-    // breadcrumbs_init = [
-    //     // {
-    //     //     title: 'Check-ins',
-    //     //     url: 'provider/check-ins'
-    //     // },
-    //     {
-    //         title: 'Customers',
-    //         url: 'provider/customers'
-    //     },
-    //     {
-    //         title: 'Add'
-    //     }
-    // ];
-    breadcrumbs;
-    // = this.breadcrumbs_init;
+    action;
+    breadcrumbs_init = [
+        // {
+        //     title: 'Check-ins',
+        //     url: 'provider/check-ins'
+        // },
+        {
+            title: 'Customers',
+            url: 'provider/customers'
+        },
+        {
+            title: 'Add'
+        }
+    ];
+    breadcrumbs = this.breadcrumbs_init;
     breadcrumb_moreoptions: any = [];
     checkin_type;
     customidFormat;
     loading = false;
-    haveMobile = true;
+    customerId;
+    customer;
+    customerName;
     constructor(
         // public dialogRef: MatDialogRef<AddProviderCustomerComponent>,
         // @Inject(MAT_DIALOG_DATA) public data: any,
@@ -73,8 +75,61 @@ export class CustomerDetailComponent implements OnInit {
         public shared_functions: SharedFunctions,
         private activated_route: ActivatedRoute,
         private _location: Location,
-        private router: Router
-    ) {
+        private router: Router) {
+             this.activated_route.params.subscribe(
+            (params) => {
+            this.customerId = params.id;
+            this.customer_label = this.shared_functions.getTerminologyTerm('customer');
+            if (this.customerId) {
+                if (this.customerId === 'add') {
+                    const breadcrumbs = [];
+                    this.breadcrumbs_init.map((e) => {
+                        breadcrumbs.push(e);
+                    });
+                    breadcrumbs.push({
+                        title: 'Add'
+                    });
+                    this.breadcrumbs = breadcrumbs;
+                    this.action = 'add';
+                    this.createForm();
+                } else {
+                    this.activated_route.queryParams.subscribe(
+                        (qParams) => {
+                            this.action = qParams.action;
+                            this.getCustomers(this.customerId).then(
+                                (customer) => {
+                                    this.customer = customer;
+                                    console.log("Customer", this.customer);
+                                    this.customerName = this.customer[0].firstName;
+                                    if (this.action === 'edit') {
+                                        const breadcrumbs = [];
+                                        this.breadcrumbs_init.map((e) => {
+                                            breadcrumbs.push(e);
+                                        });
+                                        breadcrumbs.push({
+                                            title: this.customerName
+                                        });
+                                        this.breadcrumbs = breadcrumbs;
+                                        this.createForm();
+                                    } else if (this.action === 'view') {
+                                        const breadcrumbs = [];
+                                        this.breadcrumbs_init.map((e) => {
+                                            breadcrumbs.push(e);
+                                        });
+                                        breadcrumbs.push({
+                                            title: this.customerName
+                                        });
+                                        this.breadcrumbs = breadcrumbs;
+                                    }
+                                }
+                            );
+                        }
+                    );
+                }
+                this.api_loading = false;
+            }
+        }
+    );
 
         // this.search_data = this.data.search_data;
         this.customer_label = this.shared_functions.getTerminologyTerm('customer');
@@ -89,9 +144,22 @@ export class CustomerDetailComponent implements OnInit {
             if (qparams.checkinType) {
                 this.checkin_type = qparams.checkinType;
             }
-            if (qparams.noMobile) {
-                this.haveMobile = false;
-            }
+        });
+    }
+    getCustomers(customerId){
+        const _this = this;
+        const filter = {'id-eq' : customerId}
+        return new Promise(function (resolve, reject) {
+            _this.provider_services.getProviderCustomers(filter)
+                .subscribe(
+                    data => {
+                        resolve(data);
+                        
+                    },
+                    () => {
+                        reject();
+                    }
+                );
         });
     }
 
@@ -116,9 +184,11 @@ export class CustomerDetailComponent implements OnInit {
             });
     }
     createForm() {
-        console.log(this.haveMobile);
-        if (!this.haveMobile) {
+        if (this.customidFormat && this.customidFormat.customerSeriesEnum && this.customidFormat.customerSeriesEnum === 'MANUAL') {
             this.amForm = this.fb.group({
+                customer_id: ['', Validators.compose([Validators.required])],
+                mobile_number: ['', Validators.compose([Validators.required, Validators.maxLength(10),
+                Validators.minLength(10), Validators.pattern(projectConstants.VALIDATOR_NUMBERONLY)])],
                 first_name: ['', Validators.compose([Validators.required, Validators.pattern(projectConstants.VALIDATOR_CHARONLY)])],
                 last_name: ['', Validators.compose([Validators.required, Validators.pattern(projectConstants.VALIDATOR_CHARONLY)])],
                 email_id: ['', Validators.compose([Validators.pattern(projectConstants.VALIDATOR_EMAIL)])],
@@ -138,20 +208,32 @@ export class CustomerDetailComponent implements OnInit {
                 gender: [''],
                 address: ['']
             });
+            if (this.action === 'edit') {
+                this.updateForm();
+            }
             this.loading = false;
         }
-        if (this.customidFormat && this.customidFormat.customerSeriesEnum && this.customidFormat.customerSeriesEnum === 'MANUAL') {
-        this.amForm.addControl('customer_id', new FormControl('', Validators.required));
-        }
-        if (this.phoneNo) {
-            this.amForm.get('mobile_number').setValue(this.phoneNo);
-        }
-        if (this.email) {
-            this.amForm.get('email_id').setValue(this.email);
-        }
+        // if (this.phoneNo) {
+        //     this.amForm.get('mobile_number').setValue(this.phoneNo);
+        // }
+        // if (this.email) {
+        //     this.amForm.get('email_id').setValue(this.email);
+        // }
+    }
+    updateForm() {
+        this.amForm.setValue({
+            'first_name': this.customer[0].firstName || null,
+            'last_name': this.customer[0].lastName || null,
+            'email_id' : this.customer[0].emailId || null,
+            'dob': this.customer[0].dob || null,
+            'gender': this.customer[0].gender || null,
+            'mobile_number': this.customer[0].phoneNo || null,
+            'address': this.customer[0].address || null,
+        });
     }
     onSubmit(form_data) {
         this.disableButton = true;
+        if (this.action === 'add') {
         const post_data = {
             //   'userProfile': {
             'firstName': form_data.first_name,
@@ -205,9 +287,68 @@ export class CustomerDetailComponent implements OnInit {
                 },
                 error => {
                     this.shared_functions.openSnackBar(error, { 'panelClass': 'snackbarerror' });
-                    this.disableButton = false;
                 });
+            }
+            else if (this.action === 'edit') {
+                const post_data = {
+                    //   'userProfile': {
+                    'firstName': form_data.first_name,
+                    'lastName': form_data.last_name,
+                    'dob': form_data.dob,
+                    'gender': form_data.gender,
+                    'phoneNo': form_data.mobile_number,
+                    'address': form_data.address,
+                    //   }
+                };if (form_data.mobile_number) {
+                    post_data['countryCode'] = '+91';
+                }
+                if (form_data.email_id && form_data.email_id !== '') {
+                    post_data['email'] = form_data.email_id;
+                }
+                if (form_data.customer_id) {
+                    post_data['jaldeeId'] = form_data.customer_id;
+                }
+                this.provider_services.updateProviderCustomer(post_data)
+                    .subscribe(
+                        data => {
+                            this.shared_functions.apiSuccessAutoHide(this, Messages.PROVIDER_CUSTOMER_CREATED);
+                            this.shared_functions.openSnackBar(Messages.PROVIDER_CUSTOMER_CREATED);
+                            const qParams = {};
+                            qParams['pid'] = data;
+                            if (this.source === 'checkin') {
+                                const navigationExtras: NavigationExtras = {
+                                    queryParams: {
+                                        ph: form_data.mobile_number,
+                                        checkin_type: this.checkin_type
+                                    }
+                                };
+                                this.router.navigate(['provider', 'check-ins', 'add'], navigationExtras);
+                            } else if (this.source === 'appointment') {
+                                const navigationExtras: NavigationExtras = {
+                                    queryParams: {
+                                        ph: form_data.mobile_number,
+                                        checkin_type: this.checkin_type
+                                    }
+                                };
+                                this.router.navigate(['provider', 'settings', 'appointmentmanager', 'appointments'], navigationExtras);
+                            } else {
+                                const navigationExtras: NavigationExtras = {
+                                    queryParams: {
+                                        phoneNo: this.phoneNo
+                                    }
+                                };
+                                this.router.navigate(['provider', 'customers', 'find'], navigationExtras);
+                            }
+                        },
+                        error => {
+                            this.shared_functions.openSnackBar(error, { 'panelClass': 'snackbarerror' });
+                        });
+            
+            }
+
     }
+    
+        
     onCancel() {
         this._location.back();
     }
