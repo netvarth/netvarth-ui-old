511b01de6686951e88b5b1221295807035cda021
ARUN'S PC <ARUN'S PC@DESKTOP-8S1P9SM>
2020-06-11 12:53:18 +0530

    customers bug

--- src/app/business/modules/appointments/appointments.component.ts
+++ src/app/business/modules/appointments/appointments.component.ts
@@ -1744,7 +1744,7 @@ export class AppointmentsComponent implements OnInit, OnDestroy, AfterViewInit {
     }
   }
   searchCustomer(source, appttime) {
-    this.router.navigate(['provider', 'customers', 'add'], { queryParams: { appt: true } });
+    this.router.navigate(['provider', 'customers', 'find'], { queryParams: { appt: true } });
   }
   showsheAdjustDelay() {
     if (this.queues.length === 0 || !this.selQId) {
