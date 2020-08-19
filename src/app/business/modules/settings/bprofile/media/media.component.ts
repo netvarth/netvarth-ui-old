f0b4ce5b7fbe8eef8cea5dd4ae4bdff94a8b67f4
jisy <jisy@anas>
2020-08-19 12:37:05 +0530

    local commit

--- src/app/business/modules/settings/bprofile/media/media.component.ts
+++ src/app/business/modules/settings/bprofile/media/media.component.ts
@@ -93,13 +93,15 @@ export class MediaComponent implements OnInit, OnDestroy {
         this.getGalleryImages();
         this.getBusinessProfile();
         this.subscription = this.galleryService.getMessage().subscribe(input => {
+            console.log(input.ttype);
             if (input.ttype === 'image-upload') {
+              console.log(input.ttype );
                 this.provider_services.uploadGalleryImages(input.value)
                     .subscribe(
                         () => {
-                            this.getGalleryImages();
                             this.shared_functions.openSnackBar(Messages.BPROFILE_IMAGE_UPLOAD, { 'panelClass': 'snackbarnormal' });
                             this.galleryService.sendMessage({ ttype: 'upload', status: 'success' });
+                            this.getGalleryImages();
                         },
                         error => {
                             this.shared_functions.openSnackBar(error.error, { 'panelClass': 'snackbarerror' });
@@ -125,6 +127,9 @@ export class MediaComponent implements OnInit, OnDestroy {
         if (this.delgaldialogRef) {
             this.delgaldialogRef.close();
         }
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+          }
     }
 
 
