bd2c06e75c681bd61b4c84d4a09fb741b581b5ad
jisy <jisy@anas>
2020-09-03 16:15:51 +0530

    commit

--- src/app/business/modules/teleservice/teleservice-confirm-box/teleservice-confirm-box.component.ts
+++ src/app/business/modules/teleservice/teleservice-confirm-box/teleservice-confirm-box.component.ts
@@ -10,11 +10,29 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 export class TeleServiceConfirmBoxComponent implements OnInit {
 
+  iconClass: string;
   constructor(public dialogRef: MatDialogRef<TeleServiceConfirmBoxComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
-    console.log(this.data);
+    switch (this.data.app) {
+      case 'Zoom': {
+        this.iconClass = 'fa zoom-icon';
+        break;
+      }
+      case 'GoogleMeet': {
+        this.iconClass = 'fa meet-icon';
+        break;
+      }
+      case 'WhatsApp': {
+        this.iconClass = 'a fa-whatsapp fa-lg whtsap-ic';
+        break;
+      }
+      case 'Phone': {
+        this.iconClass = 'fa fa-mobile';
+        break;
+      }
+    }
   }
   amReady() {
     this.dialogRef.close('started');
