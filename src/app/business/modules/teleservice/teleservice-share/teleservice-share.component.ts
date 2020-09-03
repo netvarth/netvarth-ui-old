9702faa99f4e6e8cdba0d158abf45c1e4c7ffb2d
unknown <smera.pr@netvarth.com>
2020-09-03 11:30:50 +0530

    style fixes

--- src/app/business/modules/teleservice/teleservice-share/teleservice-share.component.ts
+++ src/app/business/modules/teleservice/teleservice-share/teleservice-share.component.ts
@@ -27,6 +27,7 @@ export class TeleServiceShareComponent implements OnInit {
     { value: '1 Hour', viewValue: '1 Hour' }
   ];
   api_success = null;
+  providerView = false;
 
   constructor(public dialogRef: MatDialogRef<TeleServiceShareComponent>,
     public shared_functions: SharedFunctions,
@@ -50,41 +51,56 @@ export class TeleServiceShareComponent implements OnInit {
   dataChanged(evt) {
     this.msg_to_user = evt;
   }
+  tabClick(evt) {
+    if (evt.index === 1) {
+    this.providerView = true;
+    } else {
+      this.providerView = false;
+    }
+  }
   // Reminder textarea msg content
   getReminderData() {
-    switch (this.data.app) {
-      case 'WhatsApp':
-          this.msg_to_user = 'In ' + this.selectedTime + ', your ' + this.data.serviceDetail.name + ' via WhatsApp with ' + this.data.busnsName + ' will begin. Please be ready.\n\nSteps to follow to start the WhatsApp consultation: \n\n1. Wait for your turn.\n\n2. When it is time you will recieve a WhatsApp call from ' + this.data.busnsName ;
-        break;
-      case 'Phone':
+    if (this.data.status) {
+      if (this.data.app !== 'Phone') {
+        this.msg_to_user = 'Here are the details for how to start the service -\n1. Click on the following link - ' + this.data.meetingLink + '\n2. Wait for your ' + this.data.providerLabel + ' to join';
+      } else {
+        this.msg_to_user = 'Your ' + this.data.serviceDetail.name + ' with ' + this.data.busnsName + ' is on progress';
+      }
+    } else {
+      switch (this.data.app) {
+        case 'WhatsApp':
+          this.msg_to_user = 'In ' + this.selectedTime + ', your ' + this.data.serviceDetail.name + ' via WhatsApp with ' + this.data.busnsName + ' will begin. Please be ready.\n\nSteps to follow to start the WhatsApp consultation: \n\n1. Wait for your turn.\n\n2. When it is time you will recieve a WhatsApp call from ' + this.data.busnsName;
+          break;
+        case 'Phone':
           this.msg_to_user = 'In ' + this.selectedTime + ', your ' + this.data.serviceDetail.name + ' with ' + this.data.busnsName + ' will begin.\n\nYou will receive a ' + this.data.app + ' call from ' + this.data.busnsName + '. Please be ready.';
-        break;
-      case 'Zoom':
+          break;
+        case 'Zoom':
           this.msg_to_user = 'In ' + this.selectedTime + ', your ' + this.data.serviceDetail.name + ' with ' + this.data.busnsName + ' will begin. Please be ready.\n\nHere are the details for how to start the service -\n1. Click on the following link - ' + this.data.meetingLink + ' \n2. Wait for your ' + this.data.providerLabel + ' to join';
-        break;
-      case 'GoogleMeet':
+          break;
+        case 'GoogleMeet':
           this.msg_to_user = 'In ' + this.selectedTime + ', your ' + this.data.serviceDetail.name + ' with ' + this.data.busnsName + ' will begin. Please be ready.\n\nHere are the details for how to start the service -\n1. Click on the following link - ' + this.data.meetingLink + '\n2. Wait for your ' + this.data.providerLabel + ' to join';
-        break;
+          break;
+      }
     }
   }
   // Meeting detail textarea msg content
   getMeetingDetailsData() {
     switch (this.data.app) {
       case 'WhatsApp':
-          this.msg_to_user = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Click on the video icon on the top right of the screen ' ;
-          this.msg_to_me = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Click on the video icon on the top right corner of the screen to start the video consultation ' ;
+        this.msg_to_user = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Click on the video icon on the top right of the screen ';
+        this.msg_to_me = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Click on the video icon on the top right corner of the screen to start the video consultation ';
         break;
       case 'Phone':
-          this.msg_to_user = 'Call to this number ' + this.data.meetingLink;
-          this.msg_to_me = 'Call to this number ' + this.data.meetingLink;
+        this.msg_to_user = 'Call to this number ' + this.data.meetingLink;
+        this.msg_to_me = 'Call to this number ' + this.data.meetingLink;
         break;
       case 'Zoom':
-          this.msg_to_user = 'How to start the service:\n\n1. Click on the following link - ' + this.data.meetingLink + '\n2. Join meeting ' ;
-          this.msg_to_me = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Join meeting ' ;
+        this.msg_to_user = 'How to start the service:\n\n1. Click on the following link - ' + this.data.meetingLink + '\n2. Join meeting ';
+        this.msg_to_me = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Join meeting ';
         break;
       case 'GoogleMeet':
-          this.msg_to_user = 'How to start the service:\n\n1. Click on the following link - ' + this.data.meetingLink + '\n2. Click on join button ' ;
-          this.msg_to_me = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Click on join button ' ;
+        this.msg_to_user = 'How to start the service:\n\n1. Click on the following link - ' + this.data.meetingLink + '\n2. Click on join button ';
+        this.msg_to_me = 'How to start the service:\n\n 1. Click on the following link - ' + this.data.meetingLink + '\n 2. Click on join button ';
         break;
     }
   }
@@ -103,60 +119,36 @@ export class TeleServiceShareComponent implements OnInit {
       this.shared_functions.openSnackBar(Message + ' copied to clipboard');
     }
   }
-  // copyUsrMeetingDetailsInfo() {
-  //   const info = document.getElementById('usrmeetDetailsData');
-  //   if (window.getSelection) {
-  //     const selection = window.getSelection();
-  //     const range = document.createRange();
-  //     range.selectNodeContents(info);
-  //     selection.removeAllRanges();
-  //     selection.addRange(range);
-  //     document.execCommand('Copy');
-  //     this.shared_functions.openSnackBar('Meeting Details copied to clipboard');
-  //   }
-  // }
-  // copyMyMeetingDetailsInfo() {
-  //   const info = document.getElementById('mymeetDetailsData');
-  //   if (window.getSelection) {
-  //     const selection = window.getSelection();
-  //     const range = document.createRange();
-  //     range.selectNodeContents(info);
-  //     selection.removeAllRanges();
-  //     selection.addRange(range);
-  //     document.execCommand('Copy');
-  //     this.shared_functions.openSnackBar('Meeting Details copied to clipboard');
-  //   }
-  // }
 
   // Mass communication
   sendMessage() {
     const post_data = {
-        medium: {
-            email: this.email,
-            sms: this.sms,
-            pushNotification: this.pushnotify
-        },
-        communicationMessage: this.msg_to_user,
-        uuid: [this.data.waitingId]
+      medium: {
+        email: this.email,
+        sms: this.sms,
+        pushNotification: this.pushnotify
+      },
+      communicationMessage: this.msg_to_user,
+      uuid: [this.data.waitingId]
     };
     if (this.data.waitingType === 'checkin') {
-        this.shared_services.consumerMassCommunication(post_data).
-            subscribe(() => {
-              this.api_success = this.shared_functions.getProjectMesssages('PROVIDERTOCONSUMER_NOTE_ADD');
-                setTimeout(() => {
-                    this.dialogRef.close();
-                }, 2000);
-            }
-            );
+      this.shared_services.consumerMassCommunication(post_data).
+        subscribe(() => {
+          this.api_success = this.shared_functions.getProjectMesssages('PROVIDERTOCONSUMER_NOTE_ADD');
+          setTimeout(() => {
+            this.dialogRef.close();
+          }, 2000);
+        }
+        );
     } else {
-        this.shared_services.consumerMassCommunicationAppt(post_data).
-            subscribe(() => {
-              this.api_success = this.shared_functions.getProjectMesssages('PROVIDERTOCONSUMER_NOTE_ADD');
-                setTimeout(() => {
-                  this.dialogRef.close();
-              }, 2000);
-            }
-            );
+      this.shared_services.consumerMassCommunicationAppt(post_data).
+        subscribe(() => {
+          this.api_success = this.shared_functions.getProjectMesssages('PROVIDERTOCONSUMER_NOTE_ADD');
+          setTimeout(() => {
+            this.dialogRef.close();
+          }, 2000);
+        }
+        );
     }
-}
+  }
 }
