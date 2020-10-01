6335544aa405505bc9dc9f7c3ecda3667fea4e9f
Layana <layana.ts@jaldee.com>
2020-10-01 11:49:10 +0530

    ios

--- src/app/shared/constants/version.ts
+++ src/app/shared/constants/version.ts
@@ -1,8 +1,8 @@
 const versionString = {
-    'desktop' : '1.4.2',
-    // 'mobile': 'androidpro-1.3.6',
+    'desktop' : '1.4.8',
+    // 'mobile': 'androidpro-1.4.4',
     // 'updateUrl': 'https://play.google.com/store/apps/details?id=com.jaldeeinc.jaldeebusiness'
-    'mobile': 'iospro-1.4.1',
+    'mobile': 'iospro-1.4.3',
     'updateUrl': 'https://apps.apple.com/us/app/jaldee-for-business/id1475235232?ls=1'
 };
 export const version = versionString;
