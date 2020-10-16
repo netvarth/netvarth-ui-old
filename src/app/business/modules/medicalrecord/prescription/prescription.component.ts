72c9e895d2d5bc5c307a946b5d8e6504649b55d9
jisy <jisy@anas>
2020-10-16 10:13:25 +0530

    commit

--- src/app/business/modules/medicalrecord/prescription/prescription.component.ts
+++ src/app/business/modules/medicalrecord/prescription/prescription.component.ts
@@ -29,123 +29,125 @@ export class PrescriptionComponent implements OnInit {
     files: [],
     base64: [],
     caption: []
-};
-uploadlist: any = [];
+  };
+  uploadlist: any = [];
   constructor(
-  // private activatedRoot: ActivatedRoute,
-  private router: Router,
-  public dialog: MatDialog,
-  public sharedfunctionObj: SharedFunctions,
-  public provider_services: ProviderServices,
-  private medicalrecord_service: MedicalrecordService,
-   ) {
+    // private activatedRoot: ActivatedRoute,
+    private router: Router,
+    public dialog: MatDialog,
+    public sharedfunctionObj: SharedFunctions,
+    public provider_services: ProviderServices,
+    private medicalrecord_service: MedicalrecordService,
+  ) {
     this.medicalrecord_service._mrUid.subscribe(mrId => {
       this.mrId = mrId;
-      if (this.mrId !== 0) {
-        this.getMrprescription(this.mrId);
-      }
     });
 
 
-}
+  }
 
-ngOnInit() {
+  ngOnInit() {
+    if (this.mrId === 0) {
+      console.log('mrIdis zero');
 
-
-}
-uploadRx() {
-  this.router.navigate(['/provider/medicalrecord/uploadRx']);
-
-}
-
-getMrprescription(mrId) {
-  this.provider_services.getMRprescription(mrId)
-    .subscribe((data) => {
-      console.log(data);
-      if (data[0].keyName) {
-      this.uploadlist = data;
-      console.log(this.uploadlist);
     } else {
-      this.drugList = data;
+      this.getMrprescription(this.mrId);
     }
-    },
-      error => {
-        this.sharedfunctionObj.openSnackBar(this.sharedfunctionObj.getProjectErrorMesssages(error), { 'panelClass': 'snackbarerror' });
-      });
-}
 
+  }
+  uploadRx() {
+    this.router.navigate(['/provider/medicalrecord/uploadRx']);
 
-addDrug() {
-  this.addDrugdialogRef = this.dialog.open(AddDrugComponent, {
-    width: '50%',
-    panelClass: ['popup-class', 'commonpopupmainclass'],
-    disableClose: true,
-    data: {
-      type: 'add',
-      isFrom: 'manualAddrx'
-    }
-  });
+  }
 
-  this.addDrugdialogRef.afterClosed().subscribe(result => {
-    if (result) {
-      console.log(result);
-      const navigationExtras: NavigationExtras = {
-        queryParams: { details: JSON.stringify(result) }
-      };
-      this.router.navigate(['/provider/medicalrecord/addrxlist'], navigationExtras);
-    }
-  });
-}
-
-
-
-deleteDrug(index) {
-  this.drugList.splice(index, 1);
-  // delete this.drugList[index];
-}
-editDrug(drug, index) {
-  this.editedIndex = index;
-  this.addDrugdialogRef = this.dialog.open(AddDrugComponent, {
-    width: '50%',
-    panelClass: ['popup-class', 'commonpopupmainclass'],
-    disableClose: true,
-    data: {
-      type: 'edit',
-      drugDetails: drug
-    }
-  });
-
-  this.addDrugdialogRef.afterClosed().subscribe(result => {
-    if (result) {
-      this.drugList.splice(index, 1);
-      this.drugList.push(result);
-    }
-    console.log(this.drugList);
-  });
-}
-
-savePrescription() {
-  this.mrId = this.sharedfunctionObj.getitemfromLocalStorage('mrId');
-  if (this.mrId) {
-    this.provider_services.updateMRprescription(this.drugList, this.mrId).
-      subscribe(res => {
-
-      });
-  } else {
-    const passingdata = {
-      'bookingType': 'NA',
-      'consultationMode': 'EMAIL',
-      'prescriptions': this.drugList,
-      'mrConsultationDate': this.today
-    };
-    console.log(passingdata, this.userId);
-    this.provider_services.createMedicalRecord(passingdata, this.userId)
+  getMrprescription(mrId) {
+    this.provider_services.getMRprescription(mrId)
       .subscribe((data) => {
-        this.sharedfunctionObj.setitemonLocalStorage('mrId', data);
+        console.log(data);
+        if (data[0].keyName) {
+          this.uploadlist = data;
+          console.log(this.uploadlist);
+        } else {
+          this.drugList = data;
+        }
       },
         error => {
           this.sharedfunctionObj.openSnackBar(this.sharedfunctionObj.getProjectErrorMesssages(error), { 'panelClass': 'snackbarerror' });
         });
   }
-}
+
+
+  addDrug() {
+    this.addDrugdialogRef = this.dialog.open(AddDrugComponent, {
+      width: '50%',
+      panelClass: ['popup-class', 'commonpopupmainclass'],
+      disableClose: true,
+      data: {
+        type: 'add',
+        isFrom: 'manualAddrx'
+      }
+    });
+
+    this.addDrugdialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log(result);
+        const navigationExtras: NavigationExtras = {
+          queryParams: { details: JSON.stringify(result) }
+        };
+        this.router.navigate(['/provider/medicalrecord/addrxlist'], navigationExtras);
+      }
+    });
+  }
+
+
+
+  deleteDrug(index) {
+    this.drugList.splice(index, 1);
+    // delete this.drugList[index];
+  }
+  editDrug(drug, index) {
+    this.editedIndex = index;
+    this.addDrugdialogRef = this.dialog.open(AddDrugComponent, {
+      width: '50%',
+      panelClass: ['popup-class', 'commonpopupmainclass'],
+      disableClose: true,
+      data: {
+        type: 'edit',
+        drugDetails: drug
+      }
+    });
+
+    this.addDrugdialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.drugList.splice(index, 1);
+        this.drugList.push(result);
+      }
+      console.log(this.drugList);
+    });
+  }
+
+  savePrescription() {
+    this.mrId = this.sharedfunctionObj.getitemfromLocalStorage('mrId');
+    if (this.mrId) {
+      this.provider_services.updateMRprescription(this.drugList, this.mrId).
+        subscribe(res => {
+
+        });
+    } else {
+      const passingdata = {
+        'bookingType': 'NA',
+        'consultationMode': 'EMAIL',
+        'prescriptions': this.drugList,
+        'mrConsultationDate': this.today
+      };
+      console.log(passingdata, this.userId);
+      this.provider_services.createMedicalRecord(passingdata, this.userId)
+        .subscribe((data) => {
+          this.sharedfunctionObj.setitemonLocalStorage('mrId', data);
+        },
+          error => {
+            this.sharedfunctionObj.openSnackBar(this.sharedfunctionObj.getProjectErrorMesssages(error), { 'panelClass': 'snackbarerror' });
+          });
+    }
+  }
 }
