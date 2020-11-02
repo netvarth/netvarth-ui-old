7876984b2cec76a15b860adcdf0f7f19f0fc4172
jisy <jisy@anas>
2020-11-02 13:55:31 +0530

    commit

--- src/app/business/modules/medicalrecord/activity-log/activity-log.component.ts
+++ src/app/business/modules/medicalrecord/activity-log/activity-log.component.ts
@@ -11,30 +11,32 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./activity-log.component.css']
 })
 export class ActivityLogComponent implements OnInit {
+  activity_log_count: any;
   activity_log: any = [];
   auditlogs: any;
   mrId;
   // loading = true;
-  public lastVisit_dataSource = new MatTableDataSource<any>([]);
-  auditlogs_displayedColumns = ['Action', 'Description',  'Date/Time', 'userName' ];
+  public log_dataSource = new MatTableDataSource<any>([]);
+  auditlogs_displayedColumns = ['Action', 'Description', 'Date/Time', 'userName'];
   aditlogs: any = {};
 
   constructor(
-  public dialogRef: MatDialogRef<ActivityLogComponent>,
-  @Inject(MAT_DIALOG_DATA) public data: any,
-  public sharedfunctionObj: SharedFunctions,
-  public provider_services: ProviderServices,
+    public dialogRef: MatDialogRef<ActivityLogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public sharedfunctionObj: SharedFunctions,
+    public provider_services: ProviderServices,
   ) {
     this.auditlogs = this.data.activity_log;
-   // IF CONDITION
+    // IF CONDITION
     this.activity_log.push(JSON.parse(this.auditlogs.clinicalNotes));
     this.activity_log.push(JSON.parse(this.auditlogs.medicalRecord));
     this.activity_log.push(JSON.parse(this.auditlogs.prescription));
-    console.log(this.auditlogs);
+    this.activity_log_count = this.activity_log.length;
 
   }
 
   ngOnInit() {
 
 
+  }
 }
