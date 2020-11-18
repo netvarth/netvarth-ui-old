90a118e480c2cf6b55d2b7c77bd11945c4ac0dcf
Layana <layana.ts@jaldee.com>
2020-11-18 11:58:59 +0530

    appt

--- src/app/business/modules/appointments/appointments.component.ts
+++ src/app/business/modules/appointments/appointments.component.ts
@@ -2561,8 +2561,8 @@ export class AppointmentsComponent implements OnInit, OnDestroy, AfterViewInit {
           if (data[i].availableSlots) {
             for (let j = 0; j < data[i].availableSlots.length; j++) {
               if (this.apptByTimeSlot[data[i].availableSlots[j].time] || (data[i].availableSlots[j].active && data[i].availableSlots[j].noOfAvailbleSlots !== '0')) {
+                data[i].availableSlots[j]['scheduleId'] = data[i].scheduleId;
                 if (this.scheduleSlots.indexOf(data[i].availableSlots[j]) === -1) {
-                  data[i].availableSlots[j]['scheduleId'] = data[i].scheduleId;
                   this.scheduleSlots.push(data[i].availableSlots[j]);
                 }
               }
