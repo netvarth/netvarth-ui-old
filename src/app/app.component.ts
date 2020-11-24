d1373eafcbcd9427ec494b798c0c2d802fe10d7b
Layana <layana.ts@jaldee.com>
2020-11-24 17:20:08 +0530

    appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     projectConstants = this.globalService.getGlobalConstants();
     const cVersion = version.desktop;
     const pVersion = this.shared_functions.getitemfromLocalStorage('version');
-    if (!pVersion && pVersion !== cVersion) {
+    if (pVersion && pVersion !== cVersion) {
       this.shared_functions.clearLocalstorage();
       this.shared_functions.setitemonLocalStorage('version', cVersion);
     } else {
