6124e8930721dfbffa763086470eeb9758aa80c8
shiva <shiva@shivareddy-Latitude-3450>
2020-11-27 14:18:42 +0530

    changes commit

--- src/app/ynw_consumer/components/order/order.module.ts
+++ src/app/ynw_consumer/components/order/order.module.ts
@@ -8,9 +8,12 @@ import { CapitalizeFirstPipeModule } from '../../../shared/pipes/capitalize.modu
 import { LoadingSpinnerModule } from '../../../ynw_provider/components/loading-spinner/loading-spinner.module';
 import { HeaderModule } from '../../../shared/modules/header/header.module';
 import { ConsumerOrderRoutingModule } from './order.routing.module';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MaterialModule } from '../../../shared/modules/common/material.module';
 
 @NgModule({
     declarations: [
+        ShoppingCartComponent
 
     ],
     imports: [
@@ -20,7 +23,8 @@ import { ConsumerOrderRoutingModule } from './order.routing.module';
         SharedModule,
         CapitalizeFirstPipeModule,
         LoadingSpinnerModule,
-        HeaderModule
+        HeaderModule,
+        MaterialModule
     ],
     exports: [
 
