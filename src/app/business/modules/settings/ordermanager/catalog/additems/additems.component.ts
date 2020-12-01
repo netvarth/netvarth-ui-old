df8105e3e03b7cbca06eab33d6522725ae9407b7
leonora <leonora.chirayath@netvarth.com>
2020-12-01 06:40:00 +0530

    commit

--- src/app/business/modules/settings/ordermanager/catalog/additems/additems.component.ts
+++ src/app/business/modules/settings/ordermanager/catalog/additems/additems.component.ts
@@ -8,130 +8,138 @@ import { Location } from '@angular/common';
 
 
 @Component({
-    selector: 'app-additems',
-    templateUrl: './additems.component.html',
-    styleUrls: ['./additems.component.css'],
+  selector: 'app-additems',
+  templateUrl: './additems.component.html',
+  styleUrls: ['./additems.component.css'],
 })
 export class AddItemsComponent implements OnInit, OnDestroy {
-    tooltipcls = '';
-    name_cap = Messages.ITEM_NAME_CAP;
-    price_cap = Messages.PRICES_CAP;
-    taxable_cap = Messages.TAXABLE_CAP;
-    edit_btn = Messages.EDIT_BTN;
-    delete_btn = Messages.DELETE_BTN;
-    add_item_cap = Messages.ADD_ITEM_CAP;
-    item_enable_btn = Messages.ITEM_ENABLE_CAP;
-    catalogItem: any = [];
-    query_executed = false;
-    emptyMsg = '';
-    domain;
-    breadcrumb_moreoptions: any = [];
-    frm_items_cap = Messages.FRM_LEVEL_ITEMS_MSG;
-    breadcrumbs_init = [
-        {
-            url: '/provider/settings',
-            title: 'Settings'
-        },
-        {
-            title: 'Jaldee Order',
-            url: '/provider/settings/ordermanager'
-        },
-        {
-            title: 'Catalogs',
-            url: '/provider/settings/ordermanager/catalogs'
-        }
-    ];
-    item_status = projectConstants.ITEM_STATUS;
-    breadcrumbs = this.breadcrumbs_init;
-    itemnameTooltip = Messages.ITEMNAME_TOOLTIP;
-    additemdialogRef;
-    edititemdialogRef;
-    statuschangedialogRef;
-    removeitemdialogRef;
-    isCheckin;
-    active_user;
-    order = 'status';
-    selecteditems: any = [];
-    selected;
-    selectedCount = 0;
-    api_loading = true;
-    seletedCatalogItems = [];
-    tempcatalog = [];
-    constructor(private router: Router,
-        public shared_functions: SharedFunctions,
-        private location: Location,
-        private provider_servicesobj: ProviderServices) {
-        this.emptyMsg = this.shared_functions.getProjectMesssages('ITEM_LISTEMPTY');
+  tooltipcls = '';
+  name_cap = Messages.ITEM_NAME_CAP;
+  price_cap = Messages.PRICES_CAP;
+  taxable_cap = Messages.TAXABLE_CAP;
+  edit_btn = Messages.EDIT_BTN;
+  delete_btn = Messages.DELETE_BTN;
+  add_item_cap = Messages.ADD_ITEM_CAP;
+  item_enable_btn = Messages.ITEM_ENABLE_CAP;
+  catalogItem: any = [];
+  query_executed = false;
+  emptyMsg = '';
+  domain;
+  breadcrumb_moreoptions: any = [];
+  frm_items_cap = Messages.FRM_LEVEL_ITEMS_MSG;
+  breadcrumbs_init = [
+    {
+      url: '/provider/settings',
+      title: 'Settings'
+    },
+    {
+      title: 'Jaldee Order',
+      url: '/provider/settings/ordermanager'
+    },
+    {
+      title: 'Catalogs',
+      url: '/provider/settings/ordermanager/catalogs'
     }
+  ];
+  item_status = projectConstants.ITEM_STATUS;
+  breadcrumbs = this.breadcrumbs_init;
+  itemnameTooltip = Messages.ITEMNAME_TOOLTIP;
+  additemdialogRef;
+  edititemdialogRef;
+  statuschangedialogRef;
+  removeitemdialogRef;
+  isCheckin;
+  active_user;
+  order = 'status';
+  selecteditems: any = [];
+  selected;
+  selectedCount = 0;
+  api_loading = true;
+  seletedCatalogItems = [];
+  tempcatalog = [];
+  constructor(private router: Router,
+    public shared_functions: SharedFunctions,
+    private location: Location,
+    private provider_servicesobj: ProviderServices) {
+    this.emptyMsg = this.shared_functions.getProjectMesssages('ITEM_LISTEMPTY');
+  }
 
-    ngOnInit() {
-        const user = this.shared_functions.getitemFromGroupStorage('ynw-user');
-        this.domain = user.sector;
-        this.active_user = this.shared_functions.getitemFromGroupStorage('ynw-user');
-        this.breadcrumb_moreoptions = { 'actions': [{ 'title': 'Help', 'type': 'learnmore' }] };
-        this.isCheckin = this.shared_functions.getitemFromGroupStorage('isCheckin');
-        this.getitems();
-        this.seletedCatalogItems = this.shared_functions.getitemfromLocalStorage('selecteditems');
-        console.log(this.seletedCatalogItems);
-    }
-    ngOnDestroy() {
-    }
-    getitems() {
-        this.provider_servicesobj.getProviderItems()
-            .subscribe((data) => {
-                this.catalogItem = data;
-                console.log(data);
-                this.tempcatalog = this.catalogItem;
-                console.log(this.tempcatalog);
-                    console.log(this.catalogItem);
-                this.api_loading = false;
-                if (this.seletedCatalogItems) {
-                    this.selectedCount = this.seletedCatalogItems.length;
-                    for ( let i = 0; i < this.catalogItem.length; i++ ) {
-                        for ( let e = 0; e < this.seletedCatalogItems.length; e++ ) {
-                            if ( this.catalogItem[i].id === this.seletedCatalogItems[e].id ) {
-                                this.tempcatalog.splice(i, 1);
-                            }
-                        }
-                    }
-                    console.log(this.tempcatalog);
-                    console.log(this.catalogItem);
-                    //this.catalogItem = this.tempcatalog.concat(this.seletedCatalogItems);
-                    //console.log(this.catalogItem);
-                }
-            });
-    }
-    selectItem(item, index) {
-        console.log(this.catalogItem[index].selected);
-        if (this.catalogItem[index].selected === undefined || this.catalogItem[index].selected === false) {
-            this.catalogItem[index].selected = true;
-            this.selectedCount++;
-          //  this.selecteditems.push(item);
-        } else {
-            this.catalogItem[index].selected = false;
-            this.selectedCount--;
-           // this.selecteditems.splice(index, 1);
-        }
-        console.log(this.selecteditems);
-    }
-    selectedItems() {
-        this.selecteditems = [];
-        for (let ia = 0; ia < this.catalogItem.length; ia++) {
-            if (this.catalogItem[ia].selected === true) {
-              this.selecteditems.push(this.catalogItem[ia]);
+  ngOnInit() {
+    const user = this.shared_functions.getitemFromGroupStorage('ynw-user');
+    this.domain = user.sector;
+    this.active_user = this.shared_functions.getitemFromGroupStorage('ynw-user');
+    this.breadcrumb_moreoptions = { 'actions': [{ 'title': 'Help', 'type': 'learnmore' }] };
+    this.isCheckin = this.shared_functions.getitemFromGroupStorage('isCheckin');
+    this.getitems();
+    this.seletedCatalogItems = this.shared_functions.getitemfromLocalStorage('selecteditems');
+    console.log(this.seletedCatalogItems);
+  }
+  ngOnDestroy() {
+  }
+  getitems() {
+    this.provider_servicesobj.getProviderItems()
+      .subscribe((data) => {
+        this.catalogItem = data;
+          console.log('cataloITem' + JSON.stringify(this.catalogItem));
+          console.log('selectcataloITem' + JSON.stringify(this.seletedCatalogItems));
+
+        this.api_loading = false;
+        if (this.seletedCatalogItems !== null) {
+          this.catalogItem.map(item => {
+            if (this.seletedCatalogItems.includes(item.itemId)) {
+              item.selected = true;
             }
-          }
-          console.log(this.selecteditems);
-          this.shared_functions.setitemonLocalStorage('selecteditems', this.selecteditems);
-          this.location.back();
-      
-       // this.router.navigate(['provider', 'settings', 'ordermanager', 'catalogs', 'add']);
+           return item;
+          });
+          console.log('cataloITem' + JSON.stringify(this.catalogItem));
+
+        }
+        //   this.selectedCount = this.seletedCatalogItems.length;
+        //   for (let e = 0; e < this.seletedCatalogItems.length; e++) {
+        //   for (let i = 0; i < this.catalogItem.length; i++) {
+        //       if (this.catalogItem[i].id === this.seletedCatalogItems[e].id) {
+        //         this.catalogItem[i].selected = true;
+        //       }
+        //     }
+        //   }
+        //   console.log('cataloITem' + JSON.stringify(this.catalogItem));
+
+        // }
+
+
+      });
+  }
+  selectItem(item, index) {
+    console.log(this.catalogItem[index].selected);
+    if (this.catalogItem[index].selected === undefined || this.catalogItem[index].selected === false) {
+      this.catalogItem[index].selected = true;
+      this.selectedCount++;
+        this.selecteditems.push(item);
+    } else {
+      this.catalogItem[index].selected = false;
+      this.selectedCount--;
+       this.selecteditems.splice(index, 1);
     }
-    redirecToJaldeecatalogcreation() {
-        this.router.navigate(['provider', 'settings', 'ordermanager', 'catalogs', 'add']);
+    console.log(this.selecteditems);
+  }
+  selectedItems() {
+    this.selecteditems = [];
+    for (let ia = 0; ia < this.catalogItem.length; ia++) {
+      if (this.catalogItem[ia].selected === true) {
+        this.selecteditems.push(this.catalogItem[ia].itemId);
+      }
     }
-    redirecToHelp() {
-        this.router.navigate(['/provider/' + this.domain + '/billing->items']);
-    }
+    console.log(this.selecteditems);
+    this.shared_functions.setitemonLocalStorage('selecteditems', this.selecteditems);
+    this.location.back();
+
+    // this.router.navigate(['provider', 'settings', 'ordermanager', 'catalogs', 'add']);
+  }
+  redirecToJaldeecatalogcreation() {
+    this.router.navigate(['provider', 'settings', 'ordermanager', 'catalogs', 'add']);
+  }
+  redirecToHelp() {
+    this.router.navigate(['/provider/' + this.domain + '/billing->items']);
+  }
 
 }
