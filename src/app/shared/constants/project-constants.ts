1b238b7cd9fd4f9628535f9f7415d5bf15f70bb4
Layana <layana.ts@jaldee.com>
2020-12-02 20:14:45 +0530

    social

--- src/app/shared/constants/project-constants.ts
+++ src/app/shared/constants/project-constants.ts
@@ -253,7 +253,7 @@ export const projectConstantsLocal = {
     { key: 'linkedin', iconClass: 'fa fa-linkedin lkd', iconImg: 'linkedin.png', displayName: 'LinkedIn' },
     { key: 'pinterest', iconClass: 'fa fa-pinterest-square pntr', iconImg: 'pinterest.png', displayName: 'Pinterest' },
     { key: 'instagram', iconClass: 'fa fa-instagram', iconImg: 'instagram.png', displayName: 'Instagram' },
-    { key: 'BizyGlobe', iconClass: 'bizyglobe', iconImg: 'bizyglobe.png', displayName: 'BizyGlobe' },
+    { key: 'bizyGlobe', iconClass: 'BizyGlobe', iconImg: 'bizyglobe.png', displayName: 'BizyGlobe' },
     { key: 'website', iconClass: 'fa fa-globe site', iconImg: 'website.png', displayName: 'Website' }
   ],
   PRIVACY_PERMISSIONS: {
