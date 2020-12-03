4d2f697eecceedffd3e70263a6a53c96afbc3660
shiva <shiva@shivareddy-Latitude-3450>
2020-12-03 07:35:18 +0530

    Cart Timings choose

--- src/app/ynw_consumer/components/order/shopping-cart/shopping-cart.component.ts
+++ src/app/ynw_consumer/components/order/shopping-cart/shopping-cart.component.ts
@@ -16,6 +16,7 @@ export class ShoppingCartComponent implements OnInit,OnDestroy {
   orderList: any = [];
   catlog: any;
   catalogItem: any;
+  action: any = '';
 
   constructor(
     public router: Router,
@@ -88,4 +89,86 @@ export class ShoppingCartComponent implements OnInit,OnDestroy {
     this.sharedFunctionobj.setitemonLocalStorage('order', this.orderList);
     this.location.back();
   }
-}
+  goBackCart() {
+    this.action = '';
+  }
+  changeTime() {
+    this.action = 'timeChange';
+  }
+  calculateDate(days) {
+    // this.resetApi();
+    // const dte = this.sel_checkindate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const date = moment(dte, 'YYYY-MM-DD HH:mm').format();
+    // const newdate = new Date(date);
+    // newdate.setDate(newdate.getDate() + days);
+    // const dd = newdate.getDate();
+    // const mm = newdate.getMonth() + 1;
+    // const y = newdate.getFullYear();
+    // const ndate1 = y + '-' + mm + '-' + dd;
+    // const ndate = moment(ndate1, 'YYYY-MM-DD HH:mm').format();
+    // // const strtDt1 = this.hold_sel_checkindate + ' 00:00:00';
+    // const strtDt1 = this.todaydate + ' 00:00:00';
+    // const strtDt = moment(strtDt1, 'YYYY-MM-DD HH:mm').toDate();
+    // const nDt = new Date(ndate);
+    // if (nDt.getTime() >= strtDt.getTime()) {
+    //     this.sel_checkindate = ndate;
+    //     this.getQueuesbyLocationandServiceId(this.sel_loc, this.sel_ser, this.sel_checkindate, this.account_id);
+    // }
+    // const dt = this.sel_checkindate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const dt1 = moment(dt, 'YYYY-MM-DD HH:mm').format();
+    // const date1 = new Date(dt1);
+    // const dt0 = this.todaydate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const dt2 = moment(dt0, 'YYYY-MM-DD HH:mm').format();
+    // const date2 = new Date(dt2);
+    // // if (this.sel_checkindate !== this.todaydate) { // this is to decide whether future date selection is to be displayed. This is displayed if the sel_checkindate is a future date
+    // if (date1.getTime() !== date2.getTime()) { // this is to decide whether future date selection is to be displayed. This is displayed if the sel_checkindate is a future date
+    //     this.isFuturedate = true;
+    // } else {
+    //     this.isFuturedate = false;
+    // }
+    // const day1 = this.sel_checkindate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const day = moment(day1, 'YYYY-MM-DD HH:mm').format();
+    // const ddd = new Date(day);
+    // this.ddate = new Date(ddd.getFullYear() + '-' + this.sharedFunctionobj.addZero(ddd.getMonth() + 1) + '-' + this.sharedFunctionobj.addZero(ddd.getDate()));
+  }
+  disableMinus() {
+    // const seldate1 = this.sel_checkindate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const seldate2 = moment(seldate1, 'YYYY-MM-DD HH:mm').format();
+    // const seldate = new Date(seldate2);
+    // const selecttdate = new Date(seldate.getFullYear() + '-' + this.sharedFunctionobj.addZero(seldate.getMonth() + 1) + '-' + this.sharedFunctionobj.addZero(seldate.getDate()));
+    // const strtDt1 = this.hold_sel_checkindate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const strtDt2 = moment(strtDt1, 'YYYY-MM-DD HH:mm').format();
+    // const strtDt = new Date(strtDt2);
+    // const startdate = new Date(strtDt.getFullYear() + '-' + this.sharedFunctionobj.addZero(strtDt.getMonth() + 1) + '-' + this.sharedFunctionobj.addZero(strtDt.getDate()));
+    // if (startdate >= selecttdate) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+  }
+  handleFutureDateChange(e) {
+    // const tdate = e.targetElement.value;
+    // const newdate = tdate.split('/').reverse().join('-');
+    // const futrDte = new Date(newdate);
+    // const obtmonth = (futrDte.getMonth() + 1);
+    // let cmonth = '' + obtmonth;
+    // if (obtmonth < 10) {
+    //     cmonth = '0' + obtmonth;
+    // }
+    // const seldate = futrDte.getFullYear() + '-' + cmonth + '-' + futrDte.getDate();
+    // this.sel_checkindate = seldate;
+    // const dt0 = this.todaydate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const dt2 = moment(dt0, 'YYYY-MM-DD HH:mm').format();
+    // const date2 = new Date(dt2);
+    // const dte0 = this.sel_checkindate.toLocaleString(projectConstants.REGION_LANGUAGE, { timeZone: projectConstants.TIME_ZONE_REGION });
+    // const dte2 = moment(dte0, 'YYYY-MM-DD HH:mm').format();
+    // const datee2 = new Date(dte2);
+    // if (datee2.getTime() !== date2.getTime()) { // this is to decide whether future date selection is to be displayed. This is displayed if the sel_checkindate is a future date
+    //     this.isFuturedate = true;
+    // } else {
+    //     this.isFuturedate = false;
+    // }
+    // this.handleFuturetoggle();
+    // this.getQueuesbyLocationandServiceId(this.sel_loc, this.sel_ser, this.sel_checkindate, this.account_id);
+  }
+  }
