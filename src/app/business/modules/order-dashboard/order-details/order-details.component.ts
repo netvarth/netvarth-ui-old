0908c5f13c1dafec0703985b9c42253cc03e7223
Layana <layana.ts@jaldee.com>
2020-12-09 11:35:15 +0530

    order details

--- src/app/business/modules/order-dashboard/order-details/order-details.component.ts
+++ src/app/business/modules/order-dashboard/order-details/order-details.component.ts
@@ -1,6 +1,8 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { projectConstantsLocal } from '../../../../shared/constants/project-constants';
+import { SharedFunctions } from '../../../../shared/functions/shared-functions';
 import { ProviderServices } from '../../../../ynw_provider/services/provider-services.service';
 
 @Component({
@@ -11,14 +13,141 @@ import { ProviderServices } from '../../../../ynw_provider/services/provider-ser
 export class OrderDetailsComponent implements OnInit {
   uid;
   loading = false;
-  orderDetails: any = [];
+  orderDetails: any = [{
+    'uid': '6cd37128-4593-4b92-9bc7-22e130d8f576_or',
+    'orderNumber': 'o-32525t4-2',
+    'homeDelivery': true,
+    'storePickup': false,
+    'homeDeliveryAddress': 'chakaakal h,irinjalakuda po',
+    'consumer': {
+      'id': 1,
+      'firstName': 'AAA',
+      'lastName': 'AAA',
+      'favourite': false,
+      'phone_verified': false,
+      'email_verified': true,
+      'jaldeeConsumer': 4,
+      'jaldeeId': '1'
+    },
+    'jaldeeConsumer': {
+      'id': 4,
+      'favourite': false,
+      'SignedUp': false
+    },
+    'catalog': {
+      'id': 2,
+      'catalogName': 'Lunchs',
+      'catalogSchedule': {
+        'recurringType': 'Weekly',
+        'repeatIntervals': [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7'
+        ],
+        'startDate': '2020-11-20',
+        'terminator': {
+          'endDate': '2022-01-01',
+          'noOfOccurance': 0
+        },
+        'timeSlots': [
+          {
+            'sTime': '09:00 AM',
+            'eTime': '08:00 PM'
+          }
+        ]
+      },
+      'advanceAmount': 0,
+      'autoConfirm': false
+    },
+    'orderFor': {
+      'id': 1,
+      'firstName': 'AAA',
+      'lastName': 'AAA',
+      'favourite': false,
+      'phone_verified': false,
+      'email_verified': false,
+      'jaldeeConsumer': 0,
+      'jaldeeId': '1'
+    },
+    'orderItem': [
+      {
+        'id': 1,
+        'name': 'Hamlet1',
+        'quantity': 2,
+        'price': 100.22,
+        'status': 'FULFILLED',
+        'totalPrice': 200.44
+      },
+      {
+        'id': 2,
+        'name': 'Hamlet2',
+        'quantity': 1,
+        'price': 100.22,
+        'status': 'FULFILLED',
+        'totalPrice': 100.22
+      }
+    ],
+    'orderStatus': 'Accepted',
+    'orderDate': '2020-12-03',
+    'orderTimeWindow': {
+      'recurringType': 'Weekly',
+      'repeatIntervals': [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7'
+      ],
+      'startDate': '2020-11-20',
+      'terminator': {
+        'endDate': '2022-01-01',
+        'noOfOccurance': 0
+      },
+      'timeSlots': [
+        {
+          'sTime': '09:00 AM',
+          'eTime': '08:00 PM'
+        }
+      ]
+    },
+    'lastStatusUpdatedDate': '2020-12-01',
+    'isAsap': false,
+    'isFirstOrder': true,
+    'coupons': [],
+    'orderMode': 'ONLINE_ORDER',
+    'phoneNumber': '5550001023',
+    'email': 'asd@netvarth.com',
+    'advanceAmount': 0,
+    'advanceAmountToPay': 2,
+    'amount': 0,
+    'totalAmount': 0,
+    'cartAmount': 300.66,
+    'accesScope': 1,
+    'account': 0,
+    'onlineRequest': false,
+    'kioskRequest': false,
+    'firstCheckIn': false,
+    'active': false
+  }];
+  selectedType = 'list';
+  customerLabel = '';
+  display_dateFormat = projectConstantsLocal.DATE_FORMAT_WITH_MONTH;
   constructor(public activaterouter: ActivatedRoute,
     public providerservice: ProviderServices,
-    public location: Location) {
+    public location: Location, public sharedFunctions: SharedFunctions) {
     this.activaterouter.params.subscribe(param => {
       console.log(param);
       this.uid = param.id;
-      this.getOrderDetails(this.uid);
+      this.orderDetails = this.orderDetails[0];
+      console.log(this.orderDetails);
+      this.customerLabel = this.sharedFunctions.getTerminologyTerm('customer');
+      // this.getOrderDetails(this.uid);
     });
   }
 
@@ -34,5 +163,8 @@ export class OrderDetailsComponent implements OnInit {
   }
   goBack() {
     this.location.back();
+  }
+  selectViewType(type) {
+    this.selectedType = type;
   }
 }
