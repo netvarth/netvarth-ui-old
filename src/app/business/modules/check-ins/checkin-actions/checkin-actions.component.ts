bc390e9699f826821ea10817058a811bb8aa686e
Layana <layana.ts@jaldee.com>
2020-12-22 16:06:17 +0530

    terminology in bill

--- src/app/business/modules/check-ins/checkin-actions/checkin-actions.component.ts
+++ src/app/business/modules/check-ins/checkin-actions/checkin-actions.component.ts
@@ -146,7 +146,7 @@ export class CheckinActionsComponent implements OnInit {
             checkin_html += '<tr><td colspan="3" style="text-align:center">' + this.checkin.queue.location.place + '</td></tr>';
             checkin_html += '</thead><tbody>';
             if (fname !== '' || lname !== '') {
-                checkin_html += '<tr><td width="48%" align="right">Customer</td><td>:</td><td>' + fname + ' ' + lname + '</td></tr>';
+                checkin_html += '<tr><td width="48%" align="right">' + this.customer_label.charAt(0).toUpperCase() + this.customer_label.substring(1) + '</td><td>:</td><td>' + fname + ' ' + lname + '</td></tr>';
             }
             if (this.checkin.service && this.checkin.service.deptName) {
                 checkin_html += '<tr><td width="48%" align="right">Department</td><td>:</td><td>' + this.checkin.service.deptName + '</td></tr>';
