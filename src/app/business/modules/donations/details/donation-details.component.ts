acdb3c87002557850fab808b18d0a3cae08e01ad
Layana <layana.ts@jaldee.com>
2021-01-04 12:42:56 +0530

    donation details

--- src/app/business/modules/donations/details/donation-details.component.ts
+++ src/app/business/modules/donations/details/donation-details.component.ts
@@ -4,6 +4,9 @@ import { ProviderServices } from '../../../../ynw_provider/services/provider-ser
 import { SharedFunctions } from '../../../../shared/functions/shared-functions';
 import { Location } from '@angular/common';
 import { projectConstantsLocal } from '../../../../shared/constants/project-constants';
+import { ProviderWaitlistCheckInConsumerNoteComponent } from '../../check-ins/provider-waitlist-checkin-consumer-note/provider-waitlist-checkin-consumer-note.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ProviderSharedFuctions } from '../../../../ynw_provider/shared/functions/provider-shared-functions';
 
 @Component({
     'selector': 'app-donation-details',
@@ -18,7 +21,8 @@ export class DonationDetailsComponent {
     constructor(public activaterouter: ActivatedRoute,
         public sharedFunctions: SharedFunctions,
         public providerservices: ProviderServices,
-        public location: Location) {
+        public location: Location, private dialog: MatDialog,
+        public provider_shared_functions: ProviderSharedFuctions) {
         this.activaterouter.params.subscribe(param => {
             this.uid = param.id;
             this.getDonationDetails(this.uid);
@@ -33,5 +37,29 @@ export class DonationDetailsComponent {
     }
     goBack() {
         this.location.back();
+    }
+    showConsumerNote() {
+        const notedialogRef = this.dialog.open(ProviderWaitlistCheckInConsumerNoteComponent, {
+            width: '50%',
+            panelClass: ['popup-class', 'commonpopupmainclass'],
+            disableClose: true,
+            data: {
+                checkin: this.donationDetails,
+                type: 'donation'
+            }
+        });
+        notedialogRef.afterClosed().subscribe(result => {
+            if (result === 'reloadlist') {
+            }
+        });
+    }
+    addInboxMessage() {
+        let customerlist = [];
+        customerlist.push(this.donationDetails);
+        this.provider_shared_functions.ConsumerInboxMessage(customerlist, 'donation-list')
+            .then(
+                () => { },
+                () => { }
+            );
     }
 }
