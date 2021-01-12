2af792b36d2f0326c3883c91e4f249fdb4213743
Layana <layana.ts@jaldee.com>
2021-01-12 16:46:34 +0530

    validation

--- src/app/business/modules/settings/comm/update-notification/update-notification.component.ts
+++ src/app/business/modules/settings/comm/update-notification/update-notification.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Messages } from '../../../../../shared/constants/project-messages';
 import { projectConstantsLocal } from '../../../../../shared/constants/project-constants';
 import { SharedFunctions } from '../../../../../shared/functions/shared-functions';
 import { ProviderServices } from '../../../../../ynw_provider/services/provider-services.service';
@@ -33,50 +34,60 @@ export class UpdateNotificationComponent implements OnInit {
     updateVideoSettings() {
         const virtualCallingModes = [];
         let found = false;
-        this.virtualCallModesList.forEach(modes => {
-            if (modes.callingMode === this.mode) {
+        let result = true;
+        if (this.mode !== 'WhatsApp' && this.mode !== 'Phone') {
+            const pattern = new RegExp(projectConstantsLocal.VALIDATOR_URL);
+            result = pattern.test(this.callingMode);
+        }
+        if (!result) {
+            this.shared_functions.openSnackBar(Messages.BPROFILE_SOCIAL_URL_VALID, { 'panelClass': 'snackbarerror' });
+            return;
+        } else {
+            this.virtualCallModesList.forEach(modes => {
+                if (modes.callingMode === this.mode) {
+                    const mode = {
+                        'callingMode': this.mode,
+                        'value': this.callingMode,
+                        'status': 'ACTIVE'
+                    };
+                    virtualCallingModes.push(mode);
+                    found = true;
+                } else {
+                    virtualCallingModes.push(modes);
+                }
+            });
+
+            if (!found) {
                 const mode = {
                     'callingMode': this.mode,
                     'value': this.callingMode,
                     'status': 'ACTIVE'
                 };
                 virtualCallingModes.push(mode);
-                found = true;
-            } else {
-                virtualCallingModes.push(modes);
             }
-        });
-
-        if (!found) {
-            const mode = {
-                'callingMode': this.mode,
-                'value': this.callingMode,
-                'status': 'ACTIVE'
+            const postdata = {
+                'virtualCallingModes': virtualCallingModes
             };
-            virtualCallingModes.push(mode);
-        }
-        const postdata = {
-            'virtualCallingModes': virtualCallingModes
-        };
-        this.provider_services.addVirtualCallingModes(postdata).subscribe(
-            (data) => {
-                if (this.mode === 'WhatsApp') {
-                    this.shared_functions.openSnackBar('Whatsapp mode added successfully', { 'panelclass': 'snackbarerror' });
-                    this.dialogRef.close();
-                } else if (this.mode === 'Zoom') {
-                    this.shared_functions.openSnackBar('Zoom mode added successfully', { 'panelclass': 'snackbarerror' });
-                    this.dialogRef.close();
-                } else if (this.mode === 'GoogleMeet') {
-                    this.shared_functions.openSnackBar('Google Meet added successfully', { 'panelclass': 'snackbarerror' });
-                    this.dialogRef.close();
-                } else if (this.mode === 'Phone') {
-                    this.shared_functions.openSnackBar('Phone added successfully', { 'panelclass': 'snackbarerror' });
-                    this.dialogRef.close();
+            this.provider_services.addVirtualCallingModes(postdata).subscribe(
+                (data) => {
+                    if (this.mode === 'WhatsApp') {
+                        this.shared_functions.openSnackBar('Whatsapp mode added successfully', { 'panelclass': 'snackbarerror' });
+                        this.dialogRef.close();
+                    } else if (this.mode === 'Zoom') {
+                        this.shared_functions.openSnackBar('Zoom mode added successfully', { 'panelclass': 'snackbarerror' });
+                        this.dialogRef.close();
+                    } else if (this.mode === 'GoogleMeet') {
+                        this.shared_functions.openSnackBar('Google Meet added successfully', { 'panelclass': 'snackbarerror' });
+                        this.dialogRef.close();
+                    } else if (this.mode === 'Phone') {
+                        this.shared_functions.openSnackBar('Phone added successfully', { 'panelclass': 'snackbarerror' });
+                        this.dialogRef.close();
+                    }
+                },
+                error => {
+                    this.shared_functions.openSnackBar(error, { 'panelClass': 'snackbarerror' });
                 }
-            },
-            error => {
-                this.shared_functions.openSnackBar(error, { 'panelClass': 'snackbarerror' });
-            }
-        );
+            );
+        }
     }
 }
