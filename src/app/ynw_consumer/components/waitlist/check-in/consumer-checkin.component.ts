7ed7e138b777c4ee4a8cc2b2adf64439bf3489f7
Layana <layana.ts@jaldee.com>
2021-02-04 17:28:27 +0530

    image error

--- src/app/ynw_consumer/components/waitlist/check-in/consumer-checkin.component.ts
+++ src/app/ynw_consumer/components/waitlist/check-in/consumer-checkin.component.ts
@@ -1605,9 +1605,9 @@ export class ConsumerCheckinComponent implements OnInit {
         if (input) {
             for (const file of input) {
                 if (projectConstants.FILETYPES_UPLOAD.indexOf(file.type) === -1) {
-                    this.wordProcessor.apiErrorAutoHide(this, 'Selected image type not supported');
+                    this.snackbarService.openSnackBar('Selected image type not supported', { 'panelClass': 'snackbarerror' });
                 } else if (file.size > projectConstants.FILE_MAX_SIZE) {
-                    this.wordProcessor.apiErrorAutoHide(this, 'Please upload images with size < 10mb');
+                     this.snackbarService.openSnackBar('Please upload images with size < 10mb', { 'panelClass': 'snackbarerror' });
                 } else {
                     this.selectedMessage.files.push(file);
                     const reader = new FileReader();
@@ -1615,9 +1615,9 @@ export class ConsumerCheckinComponent implements OnInit {
                         this.selectedMessage.base64.push(e.target['result']);
                     };
                     reader.readAsDataURL(file);
+                    this.action = 'attachment';
                 }
             }
-            this.action = 'attachment';
         }
     }
 
