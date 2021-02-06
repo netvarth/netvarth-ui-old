62fe64c43c6d442bc6d534d3be38b2c91ba884e5
jaldeemac-ii <jaldeemac-ii@192.168.100.2>
2021-02-06 14:15:22 +0530

    prolabel

--- src/app/shared/modules/shopping-cart/checkout/checkout.component.ts
+++ src/app/shared/modules/shopping-cart/checkout/checkout.component.ts
@@ -858,7 +858,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
               uuid: this.trackUuid
             }
           };
-          if (this.catalog_details.paymentType !== 'NONE') {
+          if (this.catalog_details.paymentType !== 'NONE' || prepayAmount) {
             this.shared_services.CreateConsumerEmail(this.trackUuid, this.account_id, this.emailId)
               .subscribe(res => {
                 console.log(res);
@@ -906,7 +906,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
               uuid: this.trackUuid
             }
           };
-          if (this.catalog_details.paymentType !== 'NONE') {
+          if (this.catalog_details.paymentType !== 'NONE' || prepayAmount) {
             this.shared_services.CreateConsumerEmail(this.trackUuid, this.account_id, this.emailId)
               .subscribe(res => {
                 console.log(res);
