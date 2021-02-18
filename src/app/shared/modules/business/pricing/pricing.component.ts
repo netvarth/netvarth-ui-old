606a64f77cc608d092ae4193700949db7843d9e7
maniev <manikandan.velayudhan@netvarth.com>
2021-02-18 12:31:06 +0530

    pricing

--- src/app/shared/modules/business/pricing/pricing.component.ts
+++ src/app/shared/modules/business/pricing/pricing.component.ts
@@ -1,20 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Meta, Title } from '@angular/platform-browser';
 import { ScrollToConfigOptions, ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
+import { PricingContentDialog } from '../pricing-content-dialog/pricing-content-dialog.component';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.css']
+  styleUrls: ['./pricing.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PricingComponent implements OnInit {
+  carouselOne;
+  carouselTwo;
 
   constructor(private _scrollToService: ScrollToService,
     private titleService: Title,
     private metaService: Meta,
+    public dialog: MatDialog
     ) { }
 
   ngOnInit() {
+    this.carouselOne = {
+      nav: true,
+      navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+      dots: false,
+      loop: false,
+      autoplay: false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          center: false
+        },
+        600: {
+          items: 2,
+          center: false
+        },
+        992: {
+          items: 3,
+          center: false,
+        },
+        1300: {
+          items: 5,
+          center: false,
+        }
+      }
+    };
+    this.carouselTwo = {
+      nav: true,
+      navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+      dots: false,
+      loop: false,
+      autoplay: false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          center: false
+        },
+        600: {
+          items: 2,
+          center: false
+        },
+        992: {
+          items: 3,
+          center: false,
+        },
+        1300: {
+          items: 5,
+          center: false,
+        }
+      }
+    };
     this.titleService.setTitle('Jaldee Business - Pricing');
     this.metaService.addTags([
       { name: 'description', content: 'All first time users get a FREE 30-day Jaldee Premium plan (worth Rs.1499)' }
@@ -34,5 +92,15 @@ export class PricingComponent implements OnInit {
       offset: 0
     };
     this._scrollToService.scrollTo(config);
+  }
+    openDialog(partName) {
+      const dialogRef = this.dialog.open(PricingContentDialog, {
+        data: {
+          part: partName
+        }
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
   }
 }
