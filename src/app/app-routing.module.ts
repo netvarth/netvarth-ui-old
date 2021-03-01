bbc9982dec471d53e418dd18e061ef7f4524e6cd
Layana <layana.ts@jaldee.com>
2021-03-01 13:39:58 +0530

    label remove

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -46,7 +46,6 @@ const routes: Routes = [
     { path: 'home/:id', loadChildren: () => import('./shared/modules/about-jaldee/about-jaldee.module').then(m => m.AboutJaldeeModule) },
     { path: 'maintenance', component: MaintenanceComponent },
     { path: 'blog', component: JaldeeBlogComponent },
-    { path: ':id', component: BusinessPageComponent },
     { path: 'manage/:id', component: ManageProviderComponent },
     { path: 'status/:id', component: CheckYourStatusComponent },
     // { path: 'appt/status/:id', component: CheckYourStatusComponent },
@@ -56,6 +55,7 @@ const routes: Routes = [
     { path: 'order/shoppingcart', loadChildren: () => import ('./shared/modules/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)},
     { path: 'order/shoppingcart/checkout', loadChildren: () => import ('./shared/modules/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)},
     { path: 'order/item-details', component: ItemDetailsSharedComponent},
+    { path: ':id', component: BusinessPageComponent },
     { path: ':id/:userEncId', component: BusinessPageComponent}
     // { path: '**', redirectTo: 'not-found' }
 ];
