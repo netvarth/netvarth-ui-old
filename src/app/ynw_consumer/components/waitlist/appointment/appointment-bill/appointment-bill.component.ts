ab647db875b3ff460c5df24759515322d243e64e
basil <basil.baby@gmail.com>
2021-03-16 17:30:12 +0530

    coupon issue solved consumer side

--- src/app/ynw_consumer/components/waitlist/appointment/appointment-bill/appointment-bill.component.ts
+++ src/app/ynw_consumer/components/waitlist/appointment/appointment-bill/appointment-bill.component.ts
@@ -93,7 +93,10 @@ export class ConsumerAppointmentBillComponent implements OnInit,OnDestroy {
     showPaidlist = false;
     showJCouponSection = false;
     jCoupon = '';
-    couponList: any = [];
+    couponList : any={
+        JC:[],OWN:[]
+      };
+ 
     refund_value;
     discountDisplayNotes = false;
     billNoteExists = false;
@@ -183,6 +186,7 @@ export class ConsumerAppointmentBillComponent implements OnInit,OnDestroy {
                 data => {
                     this.checkin = data;
                     this.getCouponList();
+                    this.getproviderCouponList();
                     this.getAppointmentBill();
                     this.getPrePaymentDetails();
                     this.getPaymentModes();
@@ -700,14 +704,32 @@ export class ConsumerAppointmentBillComponent implements OnInit,OnDestroy {
                 s3Url => {
                     this.subs.sink=this.sharedServices.getbusinessprofiledetails_json(this.checkin.providerAccount.uniqueId, s3Url, 'coupon', UTCstring)
                         .subscribe(res => {
-                            this.couponList = res;
+                            
+                            this.couponList.JC = res;
+                        });
+                });
+    }
+    getproviderCouponList() {
+        const UTCstring = this.sharedfunctionObj.getCurrentUTCdatetimestring();
+        this.sharedfunctionObj.getS3Url()
+            .then(
+                s3Url => {
+                    this.subs.sink=this.sharedServices.getbusinessprofiledetails_json(this.checkin.providerAccount.uniqueId, s3Url, 'providerCoupon', UTCstring)
+                        .subscribe(res => {
+                            this.couponList.OWN = res;
                         });
                 });
     }
     checkCouponValid(couponCode) {
         let found = false;
-        for (let couponIndex = 0; couponIndex < this.couponList.length; couponIndex++) {
-            if (this.couponList[couponIndex].jaldeeCouponCode.trim() === couponCode.trim()) {
+        for (let couponIndex = 0; couponIndex < this.couponList.JC.length; couponIndex++) {
+            if (this.couponList.JC[couponIndex].jaldeeCouponCode.trim() === couponCode.trim()) {
+                found = true;
+                break;
+            }
+        }
+        for (let couponIndex = 0; couponIndex < this.couponList.OWN.length; couponIndex++) {
+            if (this.couponList.OWN[couponIndex].couponCode.trim() === couponCode.trim()) {
                 found = true;
                 break;
             }
