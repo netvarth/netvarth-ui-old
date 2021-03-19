89fc5c51ff54354a9f9631f566fc086a792ff4da
shiva <shiva@shivareddy-Latitude-3450>
2021-03-19 17:07:43 +0530

    bug fixes

--- src/app/business/modules/settings/waitlistmgr/queues/list/waitlist-queues.component.ts
+++ src/app/business/modules/settings/waitlistmgr/queues/list/waitlist-queues.component.ts
@@ -613,12 +613,15 @@ export class WaitlistQueuesComponent implements OnInit, OnDestroy {
         } else if (!this.shared_Functionsobj.checkIsInteger(instantQ.qserveonce)) {
             const error = 'Please enter an integer value for ' + this.customer_label + 's served at a time';
             this.snackbarService.openSnackBar(error, { 'panelClass': 'snackbarerror' });
-        } else if (JSON.parse(instantQ.qserveonce) === 0 || (JSON.parse(instantQ.qserveonce) > JSON.parse(instantQ.qcapacity))) {
-            const error = this.customer_label + 's' + ' ' + 'served at a time should greter than Zero';
-            // const error = this.customer_label + 's' + ' ' + 'served at a time should be lesser than Maximum' + ' ' + this.customer_label + 's served.';
+        } else if (JSON.parse(instantQ.qserveonce) === 0) {
+            const error = this.customer_label + 's' + ' ' + 'served aa at a time should greter than Zero';
             this.snackbarService.openSnackBar(error, { 'panelClass': 'snackbarerror' });
             return;
-        } else {
+        } else if ((JSON.parse(instantQ.qserveonce) > JSON.parse(instantQ.qcapacity))) {
+            const error = this.customer_label + 's' + ' ' + 'served at a time should be lesser than Maximum' + ' ' + this.customer_label + 's served.';
+            this.snackbarService.openSnackBar(error, { 'panelClass': 'snackbarerror' });
+            return;
+        }else {
             if (this.action === 'edit') {
                 this.updateInstantQ(instantQInput);
             } else {
