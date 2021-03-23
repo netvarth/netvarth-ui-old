032e9723a173152b00861f92b02c324ba6d03e8c
Layana <layana.ts@jaldee.com>
2021-03-23 17:58:54 +0530

    terms

--- src/app/shared/modules/business/signup/providersignup.routing.module.ts
+++ src/app/shared/modules/business/signup/providersignup.routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TermsStaticComponent } from '../../terms-static/terms-static.component';
 import { ProvidersignupComponent } from './providersignup.component';
 
 
 const routes: Routes = [
-    { path: '', component: ProvidersignupComponent }
+    { path: '', component: ProvidersignupComponent },
+    { path: 'terms', component: TermsStaticComponent }
     
     
     // { path: 'doctors', loadChildren: './doctors/doctors.module#DoctorsModule' },
