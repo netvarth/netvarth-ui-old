ccc1c8f17741ae3bd8cc5700d6bee3c1a4440ea7
Layana <layana.ts@jaldee.com>
2021-03-23 18:52:57 +0530

    terms

--- src/app/shared/modules/business/signup/providersignup.routing.module.ts
+++ src/app/shared/modules/business/signup/providersignup.routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TermsStaticComponent } from '../../terms-static/terms-static.component';
 import { ProvidersignupComponent } from './providersignup.component';
 
-
 const routes: Routes = [
-    { path: '', component: ProvidersignupComponent },
-    { path: 'terms', component: TermsStaticComponent }
-    
-    
-    // { path: 'doctors', loadChildren: './doctors/doctors.module#DoctorsModule' },
-    // { path: 'corporate', loadChildren: './corporate/corporate.module#CorporateModule' },
-    // { path: 'notifications', loadChildren: './notifications/notifications.module#NotificationsModule'}
+    { path: '', component: ProvidersignupComponent }
 ];
 @NgModule({
     imports: [RouterModule.forChild(routes)],
