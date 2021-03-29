8e6fbf1afe3b35a413c09ef7583c0aca168ce2bf
ARUN'S PC <ARUN'S PC@DESKTOP-8S1P9SM>
2021-03-29 12:42:48 +0530

    bug 8837

--- src/app/ynw_consumer/components/donations/details/consumer-donation.component.ts
+++ src/app/ynw_consumer/components/donations/details/consumer-donation.component.ts
@@ -819,6 +819,7 @@ export class ConsumerDonationComponent implements OnInit,OnDestroy {
     // }
     handleEmail() {
         this.action = 'email';
+        this.confrmshow = false;
         // if (this.dispCustomerEmail) {
         //     this.dispCustomerEmail = false;
         // } else {
