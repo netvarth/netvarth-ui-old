3f4d8a2c09f62df6be98163e9272555c9de853b5
shiva <shiva@shivareddy-Latitude-3450>
2021-04-09 14:39:31 +0530

    bug fixes

--- src/app/shared/functions/shared-functions.ts
+++ src/app/shared/functions/shared-functions.ts
@@ -628,7 +628,8 @@ export class SharedFunctions {
       disableClose: true,
       data: {
         'message': msg,
-        'heading': 'Public Search'
+        'heading': 'Public Search',
+        'buttons': 'okCancel'
       }
     });
 
