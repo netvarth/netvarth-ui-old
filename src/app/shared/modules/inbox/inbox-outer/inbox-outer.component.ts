a03de76d133c7ecaeba998f8c82f314605fe17a3
Layana <layana.ts@jaldee.com>
2021-04-17 15:28:31 +0530

    changes

--- src/app/shared/modules/inbox/inbox-outer/inbox-outer.component.ts
+++ src/app/shared/modules/inbox/inbox-outer/inbox-outer.component.ts
@@ -25,6 +25,7 @@ export class InboxOuterComponent implements OnInit {
   loading = false;
   message = '';
   selectedProvider = '';
+  selectedProviderName = '';
   selectedMessage = {
     files: [],
     base64: [],
@@ -94,7 +95,8 @@ export class InboxOuterComponent implements OnInit {
         data => {
           this.messages = data;
           this.scrollDone = true;
-          this.groupedMsgs = this.shared_functions.groupBy(this.messages, 'accountName');
+          this.sortMessages();
+          this.groupedMsgs = this.shared_functions.groupBy(this.messages, 'accountId');
           if (this.selectedProvider !== '') {
             this.selectedUserMessages = this.groupedMsgs[this.selectedProvider];
             const unreadMsgs = this.selectedUserMessages.filter(msg => !msg.read && msg.owner.id !== this.userDet.id);
@@ -107,7 +109,6 @@ export class InboxOuterComponent implements OnInit {
               this.scrollToElement();
             }, 100);
           }
-          this.sortMessages();
           this.shared_functions.sendMessage({ 'ttype': 'load_unread_count' });
           this.loading = false;
         },
@@ -156,6 +157,7 @@ export class InboxOuterComponent implements OnInit {
     this.clearImg();
     this.message = '';
     this.selectedProvider = msgs.key;
+    this.selectedProviderName = msgs.value[(msgs.value.length - 1)].accountName;
     this.selectedUserMessages = msgs.value;
     if (this.small_device_display) {
       this.showChat = true;
@@ -223,7 +225,9 @@ export class InboxOuterComponent implements OnInit {
       caption: []
     };
   }
-  getUserName(user) {
+  getUserName(messages) {
+    let user = messages[(messages.length-1)].accountName;
+    if (user) {
     const userPattern = new RegExp(/^[ A-Za-z0-9_.'-]*$/);
     const name = user.split(' ');
     const pattern = userPattern.test(name[0]);
@@ -232,6 +236,7 @@ export class InboxOuterComponent implements OnInit {
       nameShort = nameShort + name[name.length - 1].charAt(0);
     }
     return nameShort.toUpperCase();
+  }
   }
   filesSelected(event) {
     const input = event.target.files;
