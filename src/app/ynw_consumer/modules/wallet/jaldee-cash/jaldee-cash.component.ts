032ef23f9d20e0b6066c5590a76f1ac37bc81a3e
user <user@user-Latitude-E5440>
2021-04-27 10:47:19 +0530

    change

--- src/app/ynw_consumer/modules/wallet/jaldee-cash/jaldee-cash.component.ts
+++ src/app/ynw_consumer/modules/wallet/jaldee-cash/jaldee-cash.component.ts
@@ -4,6 +4,9 @@ import { SharedFunctions } from '../../../../shared/functions/shared-functions';
 import { SubSink } from '../../../../../../node_modules/subsink';
 import { TermsconditionComponent } from './termscondition/termsconditionpopup.component';
 import { MatDialog } from '@angular/material/dialog';
+import { SnackbarService } from '../../../../shared/services/snackbar.service';
+import { ConsumerServices } from '../../../../ynw_consumer/services/consumer-services.service';
+import { projectConstantsLocal } from '../../../../shared/constants/project-constants';
 
 
 @Component({
@@ -15,6 +18,10 @@ export class JaldeeCashComponent implements OnInit {
   private subs = new SubSink();
   screenWidth: number;
   small_device_display: boolean;
+  cashbalanceInfo:any = [];
+  cashbalanceInfodetail: any;
+  wallet_notes = projectConstantsLocal.WALLET_NOTES;
+  newDateFormat = projectConstantsLocal.DATE_EE_MM_DD_YY_FORMAT;
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.screenWidth = window.innerWidth;
@@ -26,13 +33,45 @@ export class JaldeeCashComponent implements OnInit {
   }
   constructor(private location: Location,
     private dialog: MatDialog,
+    private snackbarService: SnackbarService,
+    private consumer_services: ConsumerServices,
     public shared_functions: SharedFunctions) { }
 
   ngOnInit(): void {
-  // this.cashInfo();
+   this.cashInfo();
+   this.jaldeecashbalance();
   }
   cashInfo() {
-    throw new Error("Method not implemented.");
+    this.subs.sink=this.consumer_services.getConsumerCashbalance()
+    .subscribe(
+      data => {
+       console.log(data);
+       this.cashbalanceInfo = data;
+       
+      
+      },
+      error => {
+        this.snackbarService.openSnackBar(error, { 'panelClass': 'snackbarerror' });
+      }
+    );
+  }
+  jaldeecashbalance() {
+    this.subs.sink=this.consumer_services.getConsumerCashbalanceDetails()
+      .subscribe(
+        data => {
+         console.log(data);
+         this.cashbalanceInfodetail = data;
+        
+        //  for(let info of this.cashbalanceInfo){
+        //   this.remainamount= info.remainingAmt;
+        //  }
+         
+        
+        },
+        error => {
+          this.snackbarService.openSnackBar(error, { 'panelClass': 'snackbarerror' });
+        }
+      );
   }
   goBack () {
     this.location.back();
