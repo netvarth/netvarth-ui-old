e199f4fd9d821ef5456e20dd6f21046f846b5abb
maniev <manikandan.velayudhan@netvarth.com>
2021-05-07 20:49:18 +0530

    Meeting Issues

--- src/app/business/modules/teleservice/teleservice.module.ts
+++ src/app/business/modules/teleservice/teleservice.module.ts
@@ -5,12 +5,14 @@ import { CapitalizeFirstPipeModule } from '../../../shared/pipes/capitalize.modu
 import { TeleServiceRoutingModule } from './teleservice.routing.module';
 import { TeleServiceConfirmBoxComponent } from './teleservice-confirm-box/teleservice-confirm-box.component';
 import { TeleServiceShareComponent } from './teleservice-share/teleservice-share.component';
+import { LoadingSpinnerModule } from '../../../shared/modules/loading-spinner/loading-spinner.module';
 
 @NgModule({
     imports: [
         SharedModule,
         CapitalizeFirstPipeModule,
-        TeleServiceRoutingModule
+        TeleServiceRoutingModule,
+        LoadingSpinnerModule
     ],
     declarations: [
         TeleServiceComponent,
