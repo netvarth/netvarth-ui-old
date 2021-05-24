7630b8f21695f113a50a8308b6f3e5108253f2d4
Layana <layana.ts@jaldee.com>
2021-05-24 09:07:33 +0530

    app

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -4,9 +4,9 @@
 // const ip_url = 'http://localhost:8080/v1/rest/';
 // const ip_url = 'https://test.jaldee.com/v1/rest/';
 // const ip_url = 'http://192.168.18.30:8080/v1/rest/';
- const ip_url = 'https://scale.jaldee.com/v1/rest/';
+//  const ip_url = 'https://scale.jaldee.com/v1/rest/';
 
-// const ip_url = 'https://www.jaldee.com/v1/rest/';
+const ip_url = 'https://www.jaldee.com/v1/rest/';
 export const base_url = ip_url;
 // export const provider_url = 'https://bpc7tlo2vd.execute-api.ap-southeast-1.amazonaws.com/cloud/suggest';
 // export const search_url = 'https://bpc7tlo2vd.execute-api.ap-southeast-1.amazonaws.com/cloud/search';
