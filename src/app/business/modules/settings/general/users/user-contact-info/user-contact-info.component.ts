c7129afc97f689559c552179b5c9dff17a6b911a
ARUN'S PC <melvin.jose@netvarth.com>
2021-05-31 12:54:54 +0530

    users

---
+++ src/app/business/modules/settings/general/users/user-contact-info/user-contact-info.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+
+@Component({
+    selector: 'app-usercontact',
+    templateUrl: './user-contact-info.component.html',
+    styleUrls: ['./user-contact-info.component.css']
+})
+
+export class userContactInfoComponent implements OnInit {
+
+users_list: any = [];
+
+    constructor(
+        public dialogRef: MatDialogRef<userContactInfoComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any,
+
+    ) {
+       
+        this.users_list = data.selectedUser;
+        console.log(this.users_list)
+    }
+    ngOnInit() {
+
+    }
+}
