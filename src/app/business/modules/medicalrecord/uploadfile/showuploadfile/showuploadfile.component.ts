3335c81b91b78d61d873c00d61286108265449bb
Layana <layana.ts@jaldee.com>
2021-06-11 10:40:14 +0530

    upload file

--- src/app/business/modules/medicalrecord/uploadfile/showuploadfile/showuploadfile.component.ts
+++ src/app/business/modules/medicalrecord/uploadfile/showuploadfile/showuploadfile.component.ts
@@ -9,27 +9,36 @@ export class ShowuploadfileComponent implements OnInit {
   details: any;
   locationImg: any;
   cacheavoider = '';
-  title = '';
+  title = 'Detailed View';
   constructor(
     public dialogRef: MatDialogRef<ShowuploadfileComponent>,
     public sharedfunctionObj: SharedFunctions,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
+    console.log(this.data);
     this.details = this.data.file;
+    if (this.data.source === 'qnr') {
+      this.title = '';
+      if (this.details.s3path) {
+        this.details.url = this.details.s3path;
+      } else {
+        this.details.url = this.details.path;
+      }
+    }
+    console.log(this.details);
   }
   ngOnInit() {
-      console.log(this.details);
-      const cnow = new Date();
-      const dd = cnow.getHours() + '' + cnow.getMinutes() + '' + cnow.getSeconds();
-      this.cacheavoider = dd;
-      
+    const cnow = new Date();
+    const dd = cnow.getHours() + '' + cnow.getMinutes() + '' + cnow.getSeconds();
+    this.cacheavoider = dd;
+
   }
   showimg(imgurl) {
     let logourl = '';
-      if (imgurl) {
-        logourl = (imgurl) ? imgurl + '?' + this.cacheavoider : '';
-      }
-      return this.sharedfunctionObj.showlogoicon(logourl);
+    if (imgurl) {
+      logourl = (imgurl) ? imgurl + '?' + this.cacheavoider : '';
+    }
+    return this.sharedfunctionObj.showlogoicon(logourl);
   }
   closeDialog() {
     this.dialogRef.close();
