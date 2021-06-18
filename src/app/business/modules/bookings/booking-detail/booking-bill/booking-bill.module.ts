2d3f033da946a662f21a35d544559a21981c4229
Layana <layana.ts@jaldee.com>
2021-06-18 14:18:43 +0530

    module

--- src/app/business/modules/bookings/booking-detail/booking-bill/booking-bill.module.ts
+++ src/app/business/modules/bookings/booking-detail/booking-bill/booking-bill.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingsRoutingModule } from '../../bookings.routing.module';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { CapitalizeFirstPipeModule } from '../../../../../shared/pipes/capitalize.module';
 import { BookingBillComponent } from './booking-bill.component';
+import { LoadingSpinnerModule } from '../../../../../shared/modules/loading-spinner/loading-spinner.module';
 
 
 
@@ -14,9 +15,9 @@ import { BookingBillComponent } from './booking-bill.component';
     BookingsRoutingModule,
     MatGridListModule,
     CapitalizeFirstPipeModule,
-
+    LoadingSpinnerModule
   ],
-  exports:[BookingBillComponent]
+  exports: [BookingBillComponent]
 })
 export class BookingBillModule {
 }
