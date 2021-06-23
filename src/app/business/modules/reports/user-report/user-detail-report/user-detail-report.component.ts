6e7cb95198742e0baeebb2301f8523f7cde808a9
leonoralouis <leonora.chirayath@netvarth.com>
2021-06-23 16:05:43 +0530

    commit

--- src/app/business/modules/reports/user-report/user-detail-report/user-detail-report.component.ts
+++ src/app/business/modules/reports/user-report/user-detail-report/user-detail-report.component.ts
@@ -29,8 +29,10 @@ export class UserDetailReportComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe(
       params => {
         this.data=JSON.parse(params.data);
+        console.log(this.data);
         this.user_details_loading=false;
-        this.userStats=this.data.userDto.statsPerDay;
+        this.userStats=this.data.statsPerDay;
+        console.log(this.userStats);
       this.params=JSON.parse(params.params);
       if (this.params) {
         this.filterCriteria = this.params;
