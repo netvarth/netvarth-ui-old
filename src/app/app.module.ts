58848333d48b7b5aa5e120b1cce0dc69e5715e6d
Layana <layana.ts@jaldee.com>
2021-07-13 18:45:01 +0530

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -78,11 +78,6 @@ import { MeetRoomModule } from './shared/components/meet-room/meet-room.module';
 import { MediaService } from './shared/services/media-service';
 import { RequestDialogComponent } from './business/shared/meeting-room/request-dialog/request-dialog.component';
 import { VideoCallSharedComponent } from './business/modules/video-call/video-call.component';
-import { ContactComponent } from './shared/components/business-page-home/contactus/contact.component';
-import { TermsconditionComponent } from './shared/components/business-page-home/termscondition/termscondition.component';
-import { ShippingdeliveryComponent } from './shared/components/business-page-home/shippingdelivery/shippingdelivery.component';
-import { RefundcancelComponent } from './shared/components/business-page-home/refundcancel/refundcancel.component';
-import { PrivacyComponent } from './shared/components/business-page-home/privacy/privacy.component';
 import { FileReaderService } from './shared/services/file-reader.service';
 export function init_app(globalService: GlobalService) {
   return () => globalService.load();
@@ -103,12 +98,7 @@ export function init_app(globalService: GlobalService) {
     CommunicationComponent,
     ListRecordingsDialogComponent,
     RequestDialogComponent,
-    VideoCallSharedComponent,
-    ContactComponent,
-    TermsconditionComponent,
-    PrivacyComponent,
-    ShippingdeliveryComponent,
-    RefundcancelComponent
+    VideoCallSharedComponent
   ],
   entryComponents: [
     ConfirmBoxComponent,
