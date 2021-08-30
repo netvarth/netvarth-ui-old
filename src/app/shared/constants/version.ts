cc62e85093f7cab73a6360ee85aab5189c2ae0c6
maniev <manikandan.velayudhan@netvarth.com>
2021-08-30 15:32:54 +0530

    config version

--- src/app/shared/constants/version.ts
+++ src/app/shared/constants/version.ts
@@ -1,6 +1,6 @@
 const versionString = {
     'desktop': '1.9.13',
-    'mobile': 'androidpro-1.9.6',
+    'mobile': 'androidpro-1.9.7',
     'updateUrl': 'https://play.google.com/store/apps/details?id=com.jaldeeinc.jaldeebusiness'
     // 'mobile': 'iospro-1.9.5',
     // 'updateUrl': 'https://apps.apple.com/us/app/jaldee-for-business/id1475235232?ls=1'
