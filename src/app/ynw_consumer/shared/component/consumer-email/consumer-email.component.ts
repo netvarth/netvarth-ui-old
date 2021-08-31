8fc2d1a391eb3100fbd2845ed8e130ee516e27f9
leonoralouis <leonora.chirayath@netvarth.com>
2021-08-31 16:21:03 +0530

    commit

--- src/app/ynw_consumer/shared/component/consumer-email/consumer-email.component.ts
+++ src/app/ynw_consumer/shared/component/consumer-email/consumer-email.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+ import { SharedServices } from '../../../../shared/services/shared-services';
+import { GroupStorageService } from '../../../../shared/services/group-storage.service';
 
 
 
@@ -10,20 +12,39 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class ConsumerEmailComponent implements OnInit {
   email_id: any;
+  update_email:any;
   apiError=false;
   apiErrorTxt: any;
+  customer: any;
+  userData: any;
   constructor(public dialogRef: MatDialogRef<ConsumerEmailComponent>,
+
+     private sharedServices:SharedServices,
+    private groupService:GroupStorageService
    ) { }
 
   ngOnInit(): void {
+
+    this.userData = this.groupService.getitemFromGroupStorage('ynw-user');
+    console.log('usrData'+JSON.stringify(this.userData));
+    console.log(this.customer);
+
+    
   }
   cancel() {
     this.dialogRef.close();
   }
   onSubmit() {
     this.apiError=false;
+
     if (this.isValidEmail(this.email_id)) {
+      if(this.update_email){
+        this.updateConsumerAccount().then(result=>{
+          this.dialogRef.close(this.email_id);
+        });
+      }else{
       this.dialogRef.close(this.email_id);
+      }
     }else{
       this.apiError=true;
       this.apiErrorTxt='Please enter valid email';
@@ -33,6 +54,28 @@ export class ConsumerEmailComponent implements OnInit {
   isValidEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
+
+  }
+  updateConsumerAccount(){
+    const _this = this;
+    return new Promise(function(resolve,reject){
+      const userObj = {};
+      const firstName = _this.userData.firstName
+      const lastName = _this.userData.lastName;
+      userObj['id'] = _this.userData.id;
+      userObj['email']=_this.email_id;
+      userObj['firstName'] = firstName;
+      userObj['lastName'] = lastName;
+  
+   _this.sharedServices.updateProfile(userObj,'consumer')
+   .subscribe(data=>{
+  resolve(data);
+   }),(error)=>{
+    this.apiError=true;
+    this.apiErrorTxt=error.error;
+    reject();
+   }
+     });
 
   }
 }
