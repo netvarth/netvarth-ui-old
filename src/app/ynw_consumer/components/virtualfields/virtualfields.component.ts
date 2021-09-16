4ceeaed0066ea0ff0a8c037f47f29cdcb3b185bc
leonoralouis <leonora.chirayath@netvarth.com>
2021-09-16 11:10:05 +0530

    commit

--- src/app/ynw_consumer/components/virtualfields/virtualfields.component.ts
+++ src/app/ynw_consumer/components/virtualfields/virtualfields.component.ts
@@ -552,28 +552,36 @@ export class VirtualFieldsComponent implements OnInit {
           resolve(locations);
         },
         error => {
-          resolve([]);
+          _this.loading=false;
+          reject(error);
         }
       );
     });
   }
 
-  showLocations(event) {
+   showLocations(event) {
     let pincode = this.virtualForm.get('pincode').value;
     if (pincode.length === 6) {
       this.loading = true;
       this.fetchLocationByPincode(pincode).then(
         (locations: any) => {
           if (locations.length > 0) {
+            this.loading=false;
             this.locations = locations[0];
             this.virtualForm.patchValue({ location: locations[0]['PostOffice'][0] });
           } else {
             this.locations = [];
+          
           }
+        
+        },error=>{
+          console.log(this.loading);
           this.loading = false;
+          this.snackbarService.openSnackBar(error, { 'panelClass': 'snackbarerror' });
         }
       )
     } else {
+      this.loading=false;
       this.locations = [];
     }
   }
