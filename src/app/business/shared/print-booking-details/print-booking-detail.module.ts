f13dcfaa403fbcbdcff9a1171eb03cd4760a57b8
leonoralouis <leonora.chirayath@netvarth.com>
2021-10-22 09:01:18 +0530

    commit

--- src/app/business/shared/print-booking-details/print-booking-detail.module.ts
+++ src/app/business/shared/print-booking-details/print-booking-detail.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PrintBookingDetailsComponent } from "./print-booking-details.component";
 import { NgxQRCodeModule } from 'ngx-qrcode2';
-import { CapitalizeFirstPipeModule } from 'src/app/shared/pipes/capitalize.module';
+import { CapitalizeFirstPipeModule } from '../../../../app/shared/pipes/capitalize.module';
 const routes: Routes = [
     {path: '', component: PrintBookingDetailsComponent}
 ]
