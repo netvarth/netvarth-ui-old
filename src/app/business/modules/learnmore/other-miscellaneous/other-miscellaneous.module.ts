2828dbbefc2e85f4cd4ae1e76a0caa8cdadf3104
maniev <manikandan.velayudhan@netvarth.com>
2021-10-28 15:40:59 +0530

    Removed Shared

--- src/app/business/modules/learnmore/other-miscellaneous/other-miscellaneous.module.ts
+++ src/app/business/modules/learnmore/other-miscellaneous/other-miscellaneous.module.ts
@@ -1,17 +1,19 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { OtherMiscellaneousComponent } from "./otherMiscellaneous.component";
-import { SharedModule } from "../../../../shared/modules/common/shared.module";
-import { HeaderModule } from "../../../../shared/modules/header/header.module";
+import { MatExpansionModule } from "@angular/material/expansion";
 import { RouterModule, Routes } from "@angular/router";
+import { ScrollToModule } from "@nicky-lenaers/ngx-scroll-to";
+import { OtherMiscellaneousComponent } from "./other-miscellaneous.component";
 const routes: Routes = [
   { path: ':parent', component: OtherMiscellaneousComponent },
   { path: '/help', component: OtherMiscellaneousComponent }
 ];
 @NgModule({
     imports: [
-        SharedModule,
-        HeaderModule,
-        [RouterModule.forChild(routes)]
+      ScrollToModule.forRoot(),
+      MatExpansionModule,
+      CommonModule,
+      [RouterModule.forChild(routes)]
       ],
       declarations: [OtherMiscellaneousComponent],
       exports: [OtherMiscellaneousComponent],
