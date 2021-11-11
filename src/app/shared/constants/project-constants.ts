06171cdbf570e5d2605c09e73ccb5f858a5a6456
Layana <layana.ts@jaldee.com>
2021-11-11 15:41:44 +0530

    bugs

--- src/app/shared/constants/project-constants.ts
+++ src/app/shared/constants/project-constants.ts
@@ -210,8 +210,8 @@ export const projectConstantsLocal = {
     retailStores: { iconClass: 'retailstores_icon' },
     otherMiscellaneous: { iconClass: 'other_icon' },
     educationalInstitution: { iconClass: 'education_icon' },
-    sportsAndEntertainement: { iconClass: 'sports_icon'}
-
+    sportsAndEntertainement: { iconClass: 'sports_icon'},
+    communitySocietyAssociation: { iconClass: 'association_icon'}
   },
   DOMAIN_SERVICES_HINT: {
     healthCare: { helphint: 'In-person consultation, Video consultation, Lab services' },
