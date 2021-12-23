842915907aa0dc5ded833dc62b7ccc0cb3d50a45
Manikyam Gaddamidi <manikyamyadav1997@gmail.com>
2021-12-23 18:03:00 +0530

    Commit Changes on 12/23/21

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -4,7 +4,7 @@
 // const ip_url = 'http://localhost:8080/v1/rest/';
 const ip_url = 'https://test.jaldee.com/v1/rest/';
 //const ip_url = 'http://192.168.18.30:8080/v1/rest/';
- //const ip_url = 'https://scale.jaldee.com/v1/rest/';
+// const ip_url = 'https://scale.jaldee.com/v1/rest/';
 
 // const ip_url = 'https://www.jaldee.com/v1/rest/';
 export const base_url = ip_url;
