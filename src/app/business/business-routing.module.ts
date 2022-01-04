dcac5ee35208b591bdfc1549e5b39769504b0390
Manikyam Gaddamidi <manikyamyadav1997@gmail.com>
2022-01-04 15:58:27 +0530

    Commit Changes on 1/4/2022

--- src/app/business/business-routing.module.ts
+++ src/app/business/business-routing.module.ts
@@ -1,31 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProviderResolver } from '../ynw_provider/services/provider-resolver.service';
-import { BusinessHomeComponent } from './home/business-home.component';
+import { ProviderResolver } from './services/provider-resolver.service';
+// import { BusinessHomeComponent } from './shared/home/business-home.component';
 import { AuthGuardProviderHome, AuthGuardLogin } from '../shared/guard/auth.guard';
 import { BusinessComponent } from './business.component';
 import { EditProfileComponent } from '../shared/modules/edit-profile/edit-profile.component';
 import { AddProviderWaitlistCheckInBillComponent } from './modules/check-ins/add-provider-waitlist-checkin-bill/add-provider-waitlist-checkin-bill.component';
 import { ChangePasswordComponent } from '../shared/modules/change-password/change-password.component';
 import { ChangeMobileComponent } from '../shared/modules/change-mobile/change-mobile.component';
-import { ProviderbWizardComponent } from '../ynw_provider/components/provider-bwizard/provider-bwizard.component';
+import { ProviderbWizardComponent } from './modules/provider-bwizard/provider-bwizard.component';
 import { EnquiryComponent } from './modules/enquiry/enquiry.component';
 import { VideoCallSharedComponent } from './modules/video-call/video-call.component';
+import { BusinessPageHomeComponent } from '../shared/components/business-page-home/business-page-home.component';
 
 const routes: Routes = [
   {
     path: '', component: BusinessComponent, resolve: { terminologies: ProviderResolver },
     children: [
-      { path: '', component: BusinessHomeComponent, canActivate: [AuthGuardProviderHome] },
+      { path: '', component: BusinessPageHomeComponent, canActivate: [AuthGuardProviderHome] },
       { path: 'faq', loadChildren: () => import('./modules/faq/provider-faq.module').then(m => m.ProviderFaqModule) },
-      { path: 'customers', loadChildren: () => import('./modules/customers/customers.module').then(m => m.CustomersModule) },
+      // { path: 'customers', loadChildren: () => import('./shared/customers/customers.module').then(m => m.CustomersModule) },
+      { path: 'customers', loadChildren: () => import('./modules/customers/list/customers-list.module').then(m => m.CustomersListModule) },
+
       { path: 'telehealth', loadChildren: () => import('./modules/teleservice/teleservice.module').then(m => m.TeleServiceModule) },
       { path: 'bill/:id', component: AddProviderWaitlistCheckInBillComponent },
-      { path: 'settings', loadChildren: () => import('../ynw_provider/components/provider-settings/provider-settings.module').then(m => m.ProviderSettingsModule) },
+      { path: 'settings', loadChildren: () => import('./modules/provider-settings/provider-settings.module').then(m => m.ProviderSettingsModule) },
       { path: 'profile', component: EditProfileComponent, canActivate: [AuthGuardLogin] },
       { path: 'change-password', component: ChangePasswordComponent, canActivate: [AuthGuardLogin] },
       { path: 'change-mobile', component: ChangeMobileComponent, canActivate: [AuthGuardLogin] },
-      { path: 'inbox', loadChildren: () => import('./modules/inbox-list/inbox-list.module').then(m => m.InboxListModule) },
+      { path: 'inbox', loadChildren: () => import('./modules/inbox-list/inbox-list.module').then(m => m.BusinessInboxListModule) },
+
+      // { path: 'inbox', loadChildren: () => import('./modules/inbox-list/inbox-list.module').then(m => m.InboxListModule) },
       { path: 'finance', loadChildren: () => import('./modules/learnmore/finance/finance.module').then(m => m.FinanceModule) },
       { path: 'foodJoints', loadChildren: () => import('./modules/learnmore/foodjoints/foodjoints.module').then(m => m.FoodjointsModule) },
       { path: 'healthCare', loadChildren: () => import('./modules/learnmore/healthcare/healthcare.module').then(m => m.HealthcareModule) },
@@ -35,7 +40,9 @@ const routes: Routes = [
       { path: 'vastuAstrology', loadChildren: () => import('./modules/learnmore/vastu/vastu.module').then(m => m.VastuModule) },
       { path: 'veterinaryPetcare', loadChildren: () => import('./modules/learnmore/veterinary/veterinary.module').then(m => m.VeterinaryModule) },
       { path: 'retailStores', loadChildren: () => import('./modules/learnmore/retailstores/retailstores.module').then(m => m.RetailStoresModule) },
-      { path: 'otherMiscellaneous', loadChildren: () => import('./modules/learnmore/otherMiscellaneous/otherMiscellaneous.module').then(m => m.OtherMiscellaneousModule) },
+      { path: 'otherMiscellaneous', loadChildren: () => import('./modules/learnmore/other-miscellaneous/other-miscellaneous.module').then(m => m.OtherMiscellaneousModule) },
+
+      // { path: 'otherMiscellaneous', loadChildren: () => import('./modules/learnmore/otherMiscellaneous/otherMiscellaneous.module').then(m => m.OtherMiscellaneousModule) },
       { path: 'educationalInstitution', loadChildren: () => import('./modules/learnmore/educationInstitution/education.module').then(m => m.EducationModule) },
       { path: 'sportsAndEntertainement', loadChildren: () => import('./modules/learnmore/sports/sports.module').then(m => m.SportsModule) },
       { path: 'license', loadChildren: () => import('../business/modules/license/license.module').then(m => m.LicenseModule) },
@@ -49,7 +56,9 @@ const routes: Routes = [
       { path: 'orders', loadChildren: () => import('./modules/order-dashboard/order-dashboard.module').then(m => m.OrderDashboardModule) },
       { path: 'donations', loadChildren: () => import('./modules/donations/donations.module').then(m => m.DonationsModule) },
       { path: 'enquiry', component: EnquiryComponent },
-      { path: 'enquiry/chat', loadChildren: () => import('./modules/inbox-list/inbox-list.module').then(m => m.InboxListModule) },
+      { path: 'enquiry/chat', loadChildren: () => import('./modules/inbox-list/inbox-list.module').then(m => m.BusinessInboxListModule) },
+
+      // { path: 'enquiry/chat', loadChildren: () => import('./modules/inbox-list/inbox-list.module').then(m => m.InboxListModule) },
       { path: 'secure-video', component: VideoCallSharedComponent },
     ]
   }
