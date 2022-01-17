a77e56b4fe90d1e9dd7976569712ff50523cb6d9
maniev <manikandan.velayudhan@netvarth.com>
2022-01-17 17:16:29 +0530

    Remove Unwanted

--- src/app/business/modules/jaldee-drive/jaldee-drive.module.ts
+++ src/app/business/modules/jaldee-drive/jaldee-drive.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JaldeeDriveComponent } from './jaldee-drive/jaldee-drive.component';
-import { TableModule } from 'primeng/table';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import { MaterialModule } from '../../../shared/modules/common/material.module';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
@@ -13,37 +11,27 @@ import {NgxPaginationModule} from 'ngx-pagination';
 import { Ng2SearchPipeModule } from 'ng2-search-filter'; // import ng2search pipe module
 import { Routes, RouterModule } from '@angular/router';
 import { SharedfilesComponent } from './sharedfiles/sharedfiles.component';
-//import { CapitalizeFirstPipeModule } from 'src/app/shared/pipes/capitalize.module';
-//import {} from '../'
-
-
-
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTooltipModule } from '@angular/material/tooltip';
 const routes: Routes = [
   { path: '', component: JaldeeDriveComponent },
   { path: 'folderfiles', component: FolderFilesComponent },
   { path: 'sharedfiles', component: SharedfilesComponent }
-
-
 ];
-
-
-
 @NgModule({
   declarations: [
     JaldeeDriveComponent,
     FolderFilesComponent,
     PreviewuploadedfilesComponent,
-    SharedfilesComponent,
-
+    SharedfilesComponent
   ],
   imports: [
-    MaterialModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatTooltipModule,
     NgxIntlTelInputModule,
     NgxPaginationModule,
     Ng2SearchPipeModule,
-    
-    
     NgCircleProgressModule.forRoot({
       "radius": 60,
       "space": -10,
@@ -64,7 +52,6 @@ const routes: Routes = [
       // "responsive":false,
       "lazy": false}),
     CommonModule,
-    TableModule,  
     NgCircleProgressModule,
     MatRadioModule,
     FormsModule,
