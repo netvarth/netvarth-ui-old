57a96e57ea15cb3ab246996f9d690dd2a6265872
maniev <manikandan.velayudhan@netvarth.com>
2022-01-20 16:11:45 +0530

    ios version

--- src/app/shared/constants/version.ts
+++ src/app/shared/constants/version.ts
@@ -4,7 +4,7 @@ const versionString = {
     'iospro': 'iospro-1.6.3',
     // 'mobile': 'androidpro-1.9.11',
     // 'updateUrl': 'https://play.google.com/store/apps/details?id=com.jaldeeinc.jaldeebusiness'
-    'mobile': 'iospro-1.9.11',
+    'mobile': 'iospro-1.9.12',
     'updateUrl': 'https://apps.apple.com/us/app/jaldee-for-business/id1475235232?ls=1'
 };
 export const version = versionString;
