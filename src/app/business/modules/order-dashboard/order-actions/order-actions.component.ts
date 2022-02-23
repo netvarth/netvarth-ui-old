6dfaf47792fc64d3f9cc2d4cb6fbf8666a312078
Abhishek <Abhishek@DESKTOP-1DE8265>
2022-02-23 10:36:11 +0530

    order & customer action voice call

--- src/app/business/modules/order-dashboard/order-actions/order-actions.component.ts
+++ src/app/business/modules/order-dashboard/order-actions/order-actions.component.ts
@@ -435,7 +435,7 @@ voiceCallConfirmed() {
   // const customerId = customerDetails[0].id;
   const dialogref = this.dialog.open(VoiceConfirmComponent, {
     width: '60%',
-    height: '35%',
+    // height: '35%',
     panelClass: ['popup-class', 'commonpopupmainclass', 'confirmationmainclass'],
     disableClose: true,
     data: {
