da800bea0ba04c081c9ad3dbdf61946341701ec5
Nithesh Mohanan <nithesh@nitheshmohanan>
2022-04-11 11:06:54 +0530

    report

--- src/app/business/modules/reports/generated-report/generated-report.component.ts
+++ src/app/business/modules/reports/generated-report/generated-report.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 // import { ReportDataService } from '../reports-data.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { DateFormatPipe } from '../../../../shared/pipes/date-format/date-format.pipe';
 import { MatDialog } from '@angular/material/dialog';
 import { CriteriaDialogComponent } from './criteria-dialog/criteria-dialog.component';
@@ -9,7 +9,7 @@ import { ExportReportService } from '../export-report.service';
 // import { WordProcessor } from '../../../../shared/services/word-processor.service';
 import { ProviderServices } from '../../../../../../src/app/business/services/provider-services.service';
 import { WordProcessor } from '../../../../../../src/app/shared/services/word-processor.service';
-
+import { Location } from '@angular/common';
 export class Group {
   level = 0;
   parent: Group;
@@ -39,7 +39,7 @@ export class GeneratedReportComponent implements OnInit {
   tableColums: any;
   table_header_columns: any;
   report: any = {};
-  fff;
+  new_report;
   reducedGroups = [];
   public report_dataSource = new MatTableDataSource<any>([]);
   reprtdialogRef: any;
@@ -47,10 +47,11 @@ export class GeneratedReportComponent implements OnInit {
   groupingColumn;
   customer_label: any;
   token;
-  fo: any = [];
+  report_view: any = [];
   constructor(
     // private report_data_service: ReportDataService,
-    private router: Router,
+    // private router: Router,
+    private locationobj: Location,
     public dateformat: DateFormatPipe,
     private provider_services: ProviderServices,
     private dialog: MatDialog,
@@ -75,19 +76,19 @@ export class GeneratedReportComponent implements OnInit {
       this.provider_services.getReportListbyId(this.token)
         .subscribe(
           data => {
-            this.fff = data;
-            console.log(this.fff.status)
-            console.log(this.fff.reportContent)
-            this.fo = this.fff.reportContent.data;
-            console.log(this.fo)
-           this.tableColums = this.fff.reportContent.columns;
+            this.new_report = data;
+            console.log(this.new_report.status)
+            console.log(this.new_report.reportContent)
+            this.report_view = this.new_report.reportContent.data;
+            console.log(this.report_view)
+           this.tableColums = this.new_report.reportContent.columns;
         console.log(this.tableColums)
          this.objectKeys = Object.keys;
-        this.reportCriteriaHeader = this.fff.reportContent.reportHeader;
-        if (this.fff.reportContent && this.fff.reportContent.dataHeader) {
-          this.reportConsolidatedInfo = this.fff.reportContent.dataHeader;
+        this.reportCriteriaHeader = this.new_report.reportContent.reportHeader;
+        if (this.new_report.reportContent && this.new_report.reportContent.dataHeader) {
+          this.reportConsolidatedInfo = this.new_report.reportContent.dataHeader;
         }
-          this.reportHeader = this.fff.reportContent;
+          this.reportHeader = this.new_report.reportContent;
         Object.entries(this.tableColums).forEach(
           ([key, value]) => this.table_header.push({ 'order': key, 'name': value })
         );
@@ -97,8 +98,8 @@ export class GeneratedReportComponent implements OnInit {
             this.hide_criteria_save = true;
           }
         });
-          this.report_dataSource = this.fo;
-             if (this.fo.length === 0) {
+          this.report_dataSource = this.report_view;
+             if (this.report_view.length === 0) {
           this.showReport = false;
         } else {
           this.showReport = true;
@@ -133,7 +134,7 @@ export class GeneratedReportComponent implements OnInit {
     return this.dateformat.transformToMonthlyDate(date);
   }
   buildDataSource() {
-    this.report_dataSource = this.groupBy(this.groupingColumn, this.fo, this.reducedGroups);
+    this.report_dataSource = this.groupBy(this.groupingColumn, this.report_view, this.reducedGroups);
   }
   groupBy(column: string, data: any[], reducedGroups?: any[]) {
     if (!column) { return data; }
@@ -184,11 +185,12 @@ export class GeneratedReportComponent implements OnInit {
     this.buildDataSource();
   }
   redirecToReports() {
-    if (this.hide_criteria_save) {
-      this.router.navigate(['provider', 'reports']);
-    } else {
-      this.router.navigate(['provider', 'reports', 'new-report'], { queryParams: { report_type: this.report_type } });
-    }
+    this.locationobj.back();
+    // if (this.hide_criteria_save) {
+    //   this.router.navigate(['provider', 'reports']);
+    // } else {
+    //   this.router.navigate(['provider', 'reports', 'new-report'], { queryParams: { report_type: this.report_type } });
+    // }
   }
   saveCriteria() {
     this.reprtdialogRef = this.dialog.open(CriteriaDialogComponent, {
@@ -202,7 +204,7 @@ export class GeneratedReportComponent implements OnInit {
     });
   }
   exportReport() {
-    const reportData = this.fff.reportContent.data;
+    const reportData = this.new_report.reportContent.data;
     const tableHeader = this.tableColums;
     const _this = this;
     const reportResult = [];
