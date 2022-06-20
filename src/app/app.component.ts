a7a80a8204b0d518d522ad3cb1ba6d5b61b0c115
maniev <manikandan.velayudhan@netvarth.com>
2022-06-20 13:50:49 +0530

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,15 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { GlobalService } from './shared/services/global-service';
 import {version} from './shared/constants/version';
 import { LocalStorageService } from './shared/services/local-storage.service';
-import { TranslateService } from '@ngx-translate/core';
-// import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 export let projectConstants: any = {};
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 /**
  * Root class of Jaldee Application
  */
 export class AppComponent implements OnInit, AfterViewInit {
-  
-  // not used
-  title = 'app';
-  loading: boolean = false;
   /**
    * 
    * @param globalService 
@@ -26,30 +19,10 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   constructor(
     private globalService: GlobalService,
-    private lStorageService: LocalStorageService,
-    public translate: TranslateService,
-    // private router: Router,
-    // private ngZone:NgZone
+    private lStorageService: LocalStorageService
   ) {
     
    }
-// Shows and hides the loading spinner during RouterEvent changes
-// navigationInterceptor(event: RouterEvent): void {
-//   if (event instanceof NavigationStart) {
-//   this.ngZone.run(() => this.loading = true);
-//   }
-//   if (event instanceof NavigationEnd) {
-//     this.ngZone.run(() => this.loading = false);
-//   }
-
-//   // Set loading state to false in both of the below events to hide the spinner in case a request fails
-//   if (event instanceof NavigationCancel) {
-//     this.loading = false
-//   }
-//   if (event instanceof NavigationError) {
-//     this.loading = false
-//   }
-// }
   /**
    * Init Method
    * if version is null or different this method clears local storage items from a list.
@@ -58,11 +31,6 @@ export class AppComponent implements OnInit, AfterViewInit {
    * 
    */
   ngOnInit() {
-    // this.router.events.subscribe((e : RouterEvent) => {
-    //   this.navigationInterceptor(e);
-    // })
-    // this.lStorageService.setitemonLocalStorage('ios', true);
-    // this.lStorageService.setitemonLocalStorage('authToken', 'abcd'.toString());
     let token = this.lStorageService.getitemfromLocalStorage('authToken');
     if (token) {
       let regexToReplace = /\-/gi;
@@ -87,24 +55,5 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngAfterViewInit () {
     document.getElementById('globalLoading').remove();
   }
-  
-  // languages = [
-  //   {value: 'en', viewValue: 'English'},
-  //   {value: 'hd', viewValue: 'Hindi'},
-  //   {value: 'kan', viewValue: 'Kannada'},
-  //   {value: 'tel',viewValue:'Telugu'},
-  //   {value: 'mal',viewValue:'Malayalam'},
-  //   {value: 'tam',viewValue:'Tamil'}
-  // ];
-  //  langselected='English';
-  //  changeLocale(locale: string,languagename) {
-  //   this.langselected=languagename;
-  //   console.log('lang',this.langselected)
- 
-  //    this.translate.use(locale); 
-  
-  //     this.i18nService.changeLocale(locale);
-        
-  //  }
 }
 
