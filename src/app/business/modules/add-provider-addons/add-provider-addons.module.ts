6150765c628624e15cb9fa8d74279d42eecd7183
Manikyam Gaddamidi <manikyamyadav1997@gmail.com>
2022-07-15 11:39:11 +0530

    commit changes on 7/15/22

--- src/app/business/modules/add-provider-addons/add-provider-addons.module.ts
+++ src/app/business/modules/add-provider-addons/add-provider-addons.module.ts
@@ -6,23 +6,33 @@ import { MatOptionModule } from "@angular/material/core";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
 import { FormMessageDisplayModule } from "../../../shared/modules/form-message-display/form-message-display.module";
 import { LoadingSpinnerModule } from "../../../shared/modules/loading-spinner/loading-spinner.module";
 import { AddproviderAddonComponent } from "./add-provider-addons.component";
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @NgModule({
     imports: [
         CommonModule,
         MatDialogModule,
+        MatAutocompleteModule,
         ReactiveFormsModule,
         MatFormFieldModule,
         MatSelectModule,
         MatOptionModule,
         MatButtonModule,
         LoadingSpinnerModule,
-        FormMessageDisplayModule
+        FormMessageDisplayModule,
+        MatInputModule
     ],
     exports: [AddproviderAddonComponent],
-    declarations: [AddproviderAddonComponent]
+    declarations: [AddproviderAddonComponent],
+    providers:[
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+    ]
 })
 export class AddProviderAddonsModule {}
