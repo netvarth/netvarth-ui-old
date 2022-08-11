d280d765febd5a2e9e45ba21e64e5dce0fee829c
admin <admin@192.168.1.9>
2022-08-11 16:46:28 +0530

    commiting advance-profile changes

--- src/app/custom-app/advanced-profile/advanced-profile.component.ts
+++ src/app/custom-app/advanced-profile/advanced-profile.component.ts
@@ -9,10 +9,35 @@ export class AdvancedProfileComponent implements OnInit {
 
   @Input() businessProfile;
   @Input() templateJson;
-
+  @Input() selectedLocation;
+  emailId: any;
+  phoneNo: any;
+  location: any;
+  bLogo:any;
+  businessDesc: any;
+  specialities: any;
   constructor() { }
 
   ngOnInit(): void {
+    console.log("bprofile",this.templateJson)
+    if (this.businessProfile.logo) {
+      this.bLogo = this.businessProfile.logo.url;
+    }
+    if (this.businessProfile.emails) {
+      this.emailId = this.businessProfile.emails[0];
+    }
+    if (this.businessProfile.phoneNumbers) {
+      this.phoneNo = this.businessProfile.phoneNumbers[0];
+    }
+    if (this.businessProfile.baseLocation.place) {
+      this.location = this.businessProfile.baseLocation.place;      
+    } 
+    if (this.businessProfile.businessDesc) {
+      this.businessDesc = this.businessProfile.businessDesc;
+    }   
+    if (this.businessProfile.specialization) {
+        this.specialities = this.businessProfile.specialization;
+    }
   }
 
 }
--- src/app/custom-app/advanced-profile/advanced-profile.module.ts
+++ src/app/custom-app/advanced-profile/advanced-profile.module.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AdvancedProfileComponent } from "./advanced-profile.component";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { CapitalizeFirstPipeModule } from "../../shared/pipes/capitalize.module";
 
 @NgModule({
     declarations: [
         AdvancedProfileComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        CapitalizeFirstPipeModule,
+        MatTooltipModule
     ],
     exports: [
         AdvancedProfileComponent
