150a6b1b14643a6d10d265005dcdda22109b28fc
NARENDRA9959 <cnarendra329@gmail.com>
2022-09-02 17:28:45 +0530

    Assigned column changed in provider side

--- src/app/business/modules/order-dashboard/paper-details/paper-details.component.ts
+++ src/app/business/modules/order-dashboard/paper-details/paper-details.component.ts
@@ -4,6 +4,7 @@ import { ProviderServices } from '../../../../../../src/app/business/services/pr
 import { SubSink } from 'subsink';
 import { OrderActionsComponent } from '../order-actions/order-actions.component';
 import { Router } from '@angular/router';
+import { GroupStorageService } from '../../../../../../src/app/shared/services/group-storage.service';
 
 
 @Component({
@@ -12,20 +13,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./paper-details.component.css']
 })
 export class PaperDetailsComponent implements OnInit {
-@Input() orders;
-@Input() loading;
+  @Input() orders;
+  @Input() loading;
   selectedOrders: any;
   orderSelected: any[];
   providerLabels: any[];
-  private subs=new SubSink();
+  private subs = new SubSink();
   allLabels: any;
+  users: any;
+  selectedUser: any;
   constructor(
     private dialog: MatDialog,
     private provider_services: ProviderServices,
-    public router: Router
+    public router: Router,
+    private groupService: GroupStorageService,
+
   ) { }
 
   ngOnInit(): void {
+    this.getProviders();
   }
 
   stopprop(event) {
@@ -33,7 +39,7 @@ export class PaperDetailsComponent implements OnInit {
   }
 
 
-  showActionPopup(order?,timetype?) {
+  showActionPopup(order?, timetype?) {
     if (order) {
       this.selectedOrders = order;
     }
@@ -43,7 +49,7 @@ export class PaperDetailsComponent implements OnInit {
       disableClose: true,
       data: {
         selectedOrder: this.selectedOrders,
-        type:timetype
+        type: timetype
       }
     });
     actiondialogRef.afterClosed().subscribe(data => {
@@ -60,12 +66,33 @@ export class PaperDetailsComponent implements OnInit {
 
   getLabel() {
     this.providerLabels = [];
-    this.subs.sink=this.provider_services.getLabelList().subscribe(data => {
+    this.subs.sink = this.provider_services.getLabelList().subscribe(data => {
       this.allLabels = data;
       this.providerLabels = this.allLabels.filter(label => label.status === 'ENABLED');
     });
   }
 
+
+  getProviders() {
+    const apiFilter = {};
+    this.provider_services.getUsers(apiFilter).subscribe(data => {
+      this.users = data;
+      console.log("this.users", this.users);
+      if (this.groupService.getitemFromGroupStorage('appt-selectedUser')) {
+        this.selectedUser = this.groupService.getitemFromGroupStorage('appt-selectedUser');
+        console.log("If selected user", this.selectedUser)
+      } else {
+        if (this.users && this.users[0]) {
+          this.selectedUser = this.users[0];
+          console.log("else selected user", this.selectedUser)
+        }
+        else {
+          this.selectedUser = false;
+          console.log("else else selected user", this.selectedUser)
+        }
+      }
+    });
+  }
 
   gotoDetails(order) {
     this.router.navigate(['provider', 'orders', order.uid]);
