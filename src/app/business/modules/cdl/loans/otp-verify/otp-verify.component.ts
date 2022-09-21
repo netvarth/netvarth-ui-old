1570b54b5b96f1c19e6e0f07d1969d826130ba26
NARENDRA9959 <cnarendra329@gmail.com>
2022-09-21 12:15:15 +0530

    otp verify component changes done

--- src/app/business/modules/cdl/loans/otp-verify/otp-verify.component.ts
+++ src/app/business/modules/cdl/loans/otp-verify/otp-verify.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./otp-verify.component.css']
 })
 export class OtpVerifyComponent implements OnInit {
-
+  config = {
+    allowNumbersOnly: true,
+    length: 4,
+    inputStyles: {
+      'width': '40px',
+      'height': '40px'
+    }
+  };
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onOtpChange(event) {
+
+  }
+
+  verifyOTP() {
+
   }
 
 }
--- src/app/business/modules/cdl/loans/otp-verify/otp-verify.module.ts
+++ src/app/business/modules/cdl/loans/otp-verify/otp-verify.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OtpVerifyComponent } from './otp-verify.component';
+import { NgOtpInputModule } from 'ng-otp-input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -9,7 +11,12 @@ import { OtpVerifyComponent } from './otp-verify.component';
     OtpVerifyComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    NgOtpInputModule
+  ],
+  exports: [
+    OtpVerifyComponent
   ]
 })
 export class OtpVerifyModule { }
