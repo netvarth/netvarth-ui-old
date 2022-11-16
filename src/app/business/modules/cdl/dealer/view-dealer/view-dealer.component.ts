2411e02d98044aa73cd78de18c3ecfdd991f89b4
naren <naren@DESKTOP-C4QB9VO>
2022-11-16 13:29:10 +0530

    CDL CHANGES

--- src/app/business/modules/cdl/dealer/view-dealer/view-dealer.component.ts
+++ src/app/business/modules/cdl/dealer/view-dealer/view-dealer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CdlService } from '../../cdl.service';
 import { SnackbarService } from '../../../../../shared/services/snackbar.service';
 import { GroupStorageService } from '../../../../../shared/services/group-storage.service';
@@ -10,7 +10,7 @@ import { GroupStorageService } from '../../../../../shared/services/group-storag
   styleUrls: ['./view-dealer.component.css']
 })
 export class ViewDealerComponent implements OnInit {
-  dealerId: any;
+  dealerUid: any;
   status: any = false;
   dealerData: any;
   active: any = "inactive";
@@ -29,6 +29,11 @@ export class ViewDealerComponent implements OnInit {
   partnerLeadsCount: any = 0;
   partnerLeads: any;
   user: any;
+  dealerId: any;
+  lineChartOptions: any;
+  lineChartData: any;
+  pieChartOptions: any;
+  pieChartData: any;
   constructor(
     private location: Location,
     private router: Router,
@@ -39,7 +44,7 @@ export class ViewDealerComponent implements OnInit {
   ) {
     this.activatedroute.params.subscribe(qparams => {
       if (qparams && qparams.id) {
-        this.dealerId = qparams.id;
+        this.dealerUid = qparams.id;
       }
     });
   }
@@ -47,15 +52,23 @@ export class ViewDealerComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.groupService.getitemFromGroupStorage('ynw-user');
 
-    this.cdlservice.getDealerById(this.dealerId).subscribe(data => {
+    this.cdlservice.getDealerById(this.dealerUid).subscribe((data: any) => {
       this.dealerData = data
+      if (this.dealerData && this.dealerData.id) {
+        this.dealerId = this.dealerData.id;
+        this.getPartnerLoans();
+        this.getPartnerApprovedLoans();
+        this.getPartnerRejectedLoans();
+        this.getPartnerRejectedLoans();
+        this.getPartnerLeads();
+      }
       if (data && this.dealerData.active) {
         this.status = this.dealerData.active;
       }
       console.log("this.dealerData", this.dealerData)
     });
 
-    this.cdlservice.getDealerUsers(this.dealerId).subscribe(data => {
+    this.cdlservice.getDealerUsers(this.dealerUid).subscribe(data => {
       this.users = data
       console.log("this.users", this.users)
     });
@@ -65,11 +78,87 @@ export class ViewDealerComponent implements OnInit {
       this.customers = this.customersList.slice(0, 10);
     });
 
-    this.getPartnerLoans();
-    this.getPartnerApprovedLoans();
-    this.getPartnerRejectedLoans();
-    this.getPartnerRejectedLoans();
-    this.getPartnerLeads();
+    this.getLinearChartData();
+  }
+
+
+  getPieChartData() {
+    this.pieChartOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#1E4079'
+          }
+        }
+      }
+    };
+
+
+    this.pieChartData = {
+      labels: ['Leads', 'Approved', 'Pending', 'Rejected'],
+      datasets: [
+        {
+          data: [this.partnerLeadsCount, this.totalApprovedCount, this.ApprovedPendingloans, this.RejectedLoansCount],
+          backgroundColor: [
+            "orange",
+            "#3CB698",
+            "#FF9567",
+            "#F96565"
+          ],
+          hoverBackgroundColor: [
+            "#64B5F6",
+            "#64B5F6",
+            "#64B5F6",
+            "#64B5F6"
+          ]
+        }
+      ]
+    };
+  }
+
+
+  getLinearChartData() {
+    this.lineChartData = {
+      labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      datasets: [
+        {
+          label: 'Loans Analytics',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: true,
+          borderColor: 'rgba(202, 30, 30, 1)',
+          tension: .4,
+          backgroundColor: 'rgba(238, 108, 77, 0.2)'
+        }
+      ]
+    }
+
+    this.lineChartOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#1E4079'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#1E4079'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#1E4079'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        }
+      },
+    };
   }
 
 
@@ -80,10 +169,13 @@ export class ViewDealerComponent implements OnInit {
   getPartnerLoans() {
     const api_filter = {};
     api_filter['partner-eq'] = this.dealerId;
+    console.log('Dealer Id', this.dealerId)
     this.cdlservice.getLoansByFilter(api_filter).subscribe((data: any) => {
       this.statusLoansList = data;
       this.loans = data;
       this.totalLoansCount = data.length;
+      this.getPieChartData();
+
     })
   }
 
@@ -95,6 +187,8 @@ export class ViewDealerComponent implements OnInit {
       this.statusLoansList = data;
       this.Approvedloans = data;
       this.totalApprovedCount = data.length;
+      this.getPieChartData();
+
     })
   }
 
@@ -106,6 +200,8 @@ export class ViewDealerComponent implements OnInit {
       this.statusLoansList = data;
       this.ApprovedPendingloans = data;
       this.totalApprovedPendingCount = data.length;
+      this.getPieChartData();
+
     })
   }
 
@@ -118,17 +214,20 @@ export class ViewDealerComponent implements OnInit {
       this.statusLoansList = data;
       this.partnerLeads = data;
       this.partnerLeadsCount = data.length;
+      this.getPieChartData();
+
     })
   }
   getPartnerRejectedLoans() {
     const api_filter = {};
     api_filter['partner-eq'] = this.dealerId;
-    api_filter['applicatrionStatus-eq'] = "Rejected";
+    api_filter['applicationStatus-eq'] = "Rejected";
     this.cdlservice.getLoansByFilter(api_filter).subscribe((data: any) => {
       this.statusLoansList = data;
       this.RejectedLoans = data;
       this.RejectedLoansCount = data.length;
       console.log("Loans List : ", this.statusLoansList);
+      this.getPieChartData();
     })
   }
 
@@ -142,7 +241,7 @@ export class ViewDealerComponent implements OnInit {
 
   changeActive(event) {
     let statusChange = (event.checked) ? true : false;
-    this.cdlservice.partnerAccountStatus(this.dealerId, statusChange).subscribe((data: any) => {
+    this.cdlservice.partnerAccountStatus(this.dealerUid, statusChange).subscribe((data: any) => {
       if (data) {
         this.status = (event.checked) ? true : false;
         let statusDisplayName = this.status ? 'Active' : 'Inactive';
@@ -156,17 +255,7 @@ export class ViewDealerComponent implements OnInit {
   }
 
   loanDetails(data) {
-    console.log(data);
-    const status = data['status'];
-    const customerName = data['CustomerName']
-    const navigationExtras: NavigationExtras = {
-      queryParams: {
-        type: 'loanDetails',
-        status: status,
-        customerName: customerName
-      }
-    };
-    this.router.navigate(['provider', 'cdl', 'loans', 'loanDetails'], navigationExtras);
+    this.router.navigate(['provider', 'cdl', 'loans', data]);
   }
   dealerApproved() {
     this.router.navigate(['provider', 'cdl', 'dealers']);
--- src/app/business/modules/cdl/dealer/view-dealer/view-dealer.module.ts
+++ src/app/business/modules/cdl/dealer/view-dealer/view-dealer.module.ts
@@ -5,6 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { TabViewModule } from 'primeng/tabview';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ChartModule } from 'primeng/chart';
 
 const routes: Routes = [
   { path: '', component: ViewDealerComponent }
@@ -20,6 +26,12 @@ const routes: Routes = [
     MatInputModule,
     FormsModule,
     MatSlideToggleModule,
+    TabViewModule,
+    TableModule,
+    CalendarModule,
+    DropdownModule,
+    InputTextModule,
+    ChartModule,
     [RouterModule.forChild(routes)]
   ],
   exports: [
