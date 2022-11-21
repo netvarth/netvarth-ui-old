9897c00ae22ebc60115e45610dd21bba6d5b4f09
naren <naren@192.168.1.203>
2022-11-21 19:24:55 +0530

    CDL CHANGES

--- src/app/business/modules/cdl/emi-calci/emi-calci.component.ts
+++ src/app/business/modules/cdl/emi-calci/emi-calci.component.ts
@@ -8,11 +8,58 @@ import { Location } from '@angular/common';
 })
 export class EmiCalciComponent implements OnInit {
 
+  loanAmount: any = 5000;
+  interestRate: any = 8;
+  loanTenure: any = 6;
+  monthlyInterest: any = 0;
+  EmiPayment: any = 0;
+  pieChartOptions: any;
+  pieChartData: any;
   constructor(
     private location: Location
   ) { }
 
   ngOnInit(): void {
+    this.calculateEmi();
+    this.getPieChartData();
+  }
+
+  getPieChartData() {
+    this.pieChartOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#1E4079'
+          }
+        }
+      }
+    };
+
+
+    this.pieChartData = {
+      labels: ['Loan Amount', 'Interest'],
+      datasets: [
+        {
+          data: [this.loanAmount, this.monthlyInterest * this.loanTenure],
+          backgroundColor: [
+            "orange",
+            "#3CB698"
+          ],
+          hoverBackgroundColor: [
+            "#64B5F6",
+            "#64B5F6"
+          ]
+        }
+      ]
+    };
+  }
+
+  calculateEmi() {
+    console.log(this.loanAmount, this.interestRate, this.EmiPayment)
+    this.monthlyInterest = (this.loanAmount * (this.interestRate * 0.01)) / this.loanTenure;
+    this.EmiPayment = ((this.loanAmount / this.loanTenure) + this.monthlyInterest).toFixed(2);
+    console.log(this.monthlyInterest, this.EmiPayment);
+    this.getPieChartData();
   }
 
   goBack() {
--- src/app/business/modules/cdl/emi-calci/emi-calci.module.ts
+++ src/app/business/modules/cdl/emi-calci/emi-calci.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmiCalciComponent } from './emi-calci.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { SliderModule } from 'primeng/slider';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ChartModule } from 'primeng/chart';
 
 const routes: Routes = [
   { path: '', component: EmiCalciComponent }
@@ -15,6 +19,11 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    SliderModule,
+    InputTextModule,
+    FormsModule,
+    InputNumberModule,
+    ChartModule,
     [RouterModule.forChild(routes)]
   ]
 })
