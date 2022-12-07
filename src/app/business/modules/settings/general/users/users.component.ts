e875c91a709600fc5b3d0dd4af15ce5c479b5b1f
naren <naren@DESKTOP-C4QB9VO>
2022-12-07 14:44:22 +0530

    CDL CHANGES

--- src/app/business/modules/settings/general/users/users.component.ts
+++ src/app/business/modules/settings/general/users/users.component.ts
@@ -325,9 +325,11 @@ export class BranchUsersComponent implements OnInit {
                                 return user
                             })
                             _this.users_list.map(function (user) {
-                                const businessLocations = _this.locationsjson.filter(loc => user.userType === 'PROVIDER' && user.bussLocations ? !(user.bussLocations.indexOf(loc.id) < 0) : "");
-                                user.businessLocations = businessLocations.map(loc => loc.place);
-                                return user
+                                if (user && user.bussLocations) {
+                                    const businessLocations = _this.locationsjson.filter(loc => user.userType === 'PROVIDER' && user.bussLocations ? !(user.bussLocations.indexOf(loc.id) < 0) : "");
+                                    user.businessLocations = businessLocations.map(loc => loc.place);
+                                    return user
+                                }
                             })
 
                             // this.users_list = this.users_list.map(function(user) {
