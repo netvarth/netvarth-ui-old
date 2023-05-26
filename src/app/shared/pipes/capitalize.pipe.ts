f71bca0915c9eac352c89cfbba033e28c9819cf6
Narendra <cnarendra329@gmail.com>
2023-05-26 18:59:31 +0530

    CDL CHANGES

--- src/app/shared/pipes/capitalize.pipe.ts
+++ src/app/shared/pipes/capitalize.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'capitalizeFirst'})
+@Pipe({ name: 'capitalizeFirst' })
 export class CapitalizeFirstPipe implements PipeTransform {
-    transform(value: string): string {
-      if (value !== null && value !== undefined ) {
-        value = value.toString().trim();
-        return value.charAt(0).toUpperCase() + value.slice(1);
-      }
+  transform(value: string): string {
+    if (value !== null && value !== undefined) {
+      value = value.toString().trim();
+      return value.charAt(0).toUpperCase() + value.slice(1);
     }
   }
+}
